645f940ce1ddad98ee85f6378ee4073dd951ac65
Barry Song <Baohua.Song@csr.com>
2011-08-23 01:58:19 -0700

    [!no_src_qa!]CSR: temp rootfs, build/flash utilities for Linux 3.0
    Signed-off-by: Barry Song <Baohua.Song@csr.com>

---
+++ build.sh
@@ -0,0 +1,8 @@
+export ARCH=arm
+export EXTRADIR=${PWD}/extra
+make prima2cb_defconfig
+make uImage
+make dtbs
+cp arch/arm/boot/uImage extra
+cp arch/arm/boot/prima2-cb.dtb	extra/dtb
+cp .config	extra/
