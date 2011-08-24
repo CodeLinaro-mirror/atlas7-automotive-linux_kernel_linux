74de071df4934a5d38600a2fba3ee214ab6464c3
Barry Song <Baohua.Song@csr.com>
2011-08-23 19:49:59 -0700

    ARM: CSR: RTCIOBRG: minor cleanup
    Signed-off-by: Barry Song <Baohua.Song@csr.com>

--- include/linux/rtc/sirfsoc_rtciobrg.h
+++ include/linux/rtc/sirfsoc_rtciobrg.h
@@ -9,12 +9,10 @@
 #ifndef _SIRFSOC_RTC_IOBRG_H_
 #define _SIRFSOC_RTC_IOBRG_H_
 
-#ifndef __ASSEMBLY__
 extern void sirfsoc_rtc_iobrg_besyncing(void);
 
 extern u32 sirfsoc_rtc_iobrg_readl(u32 addr);
-                             
+
 extern void sirfsoc_rtc_iobrg_writel(u32 val, u32 addr);
-#endif
 
 #endif
