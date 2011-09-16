e45a4dc769c0bed4765f50300275d219b03d792d
Barry Song <Baohua.Song@csr.com>
2011-09-15 19:05:33 -0700

    ARM: CSR: l2: add prima2-specific string in dts
    Signed-off-by: Barry Song <Baohua.Song@csr.com>

--- arch/arm/mach-prima2/l2x0.c
+++ arch/arm/mach-prima2/l2x0.c
@@ -8,10 +8,24 @@
 
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/of.h>
 #include <asm/hardware/cache-l2x0.h>
+
+static struct of_device_id prima2_l2x0_ids[]  = {
+	{ .compatible = "sirf,prima2-pl310-cache" },
+	{},
+};
 
 static int __init sirfsoc_l2x0_init(void)
 {
-	return l2x0_of_init(0x40000, 0);
+	struct device_node *np;
+
+	np = of_find_matching_node(NULL, prima2_l2x0_ids);
+	if (np) {
+		pr_info("Initializing prima2 L2 cache\n");
+		return l2x0_of_init(0x40000, 0);
+	}
+
+	return 0;
 }
 early_initcall(sirfsoc_l2x0_init);
