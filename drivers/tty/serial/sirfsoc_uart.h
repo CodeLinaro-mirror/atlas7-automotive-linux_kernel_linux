bfb6d43371e888dac0e8d56d07bbb89032c2673c
Barry Song <Barry.Song@csr.com>
2011-10-13 20:36:55 -0700

    UART: SIRF: rename ttyS and use dynamic device number
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- drivers/tty/serial/sirfsoc_uart.h
+++ drivers/tty/serial/sirfsoc_uart.h
@@ -134,9 +134,9 @@
 #define SIRFUART_TX_EN				0x2
 
 /* Generic Definitions */
-#define SIRFSOC_UART_NAME			"ttyS"
-#define SIRFSOC_UART_MAJOR			TTY_MAJOR
-#define SIRFSOC_UART_MINOR			64
+#define SIRFSOC_UART_NAME			"ttySiRF"
+#define SIRFSOC_UART_MAJOR			0
+#define SIRFSOC_UART_MINOR			0
 #define SIRFUART_PORT_NAME			"sirfsoc-uart"
 #define SIRFUART_MAP_SIZE			0x200
 #define SIRFSOC_UART_NR				3
