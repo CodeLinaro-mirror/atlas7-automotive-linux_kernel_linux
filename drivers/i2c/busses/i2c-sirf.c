5d6eea9fbc0caf489c5e30c870bc61187dc6e2be
Barry Song <Barry.Song@csr.com>
2011-11-17 21:51:59 -0800

    I2C: SIRF: delete redundant head files
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- drivers/i2c/busses/i2c-sirf.c
+++ drivers/i2c/busses/i2c-sirf.c
@@ -10,8 +10,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/slab.h>
-#include <linux/init.h>
-#include <linux/sched.h>
 #include <linux/platform_device.h>
 #include <linux/i2c.h>
 #include <linux/clk.h>
