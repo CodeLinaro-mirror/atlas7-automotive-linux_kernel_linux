89ef9dfb6462f2043361a43b75f12917dbaef8c3
Barry Song <Barry.Song@csr.com>
2011-12-01 16:12:47 +0800

    build.sh: enable 8 threads compiling
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ export ARCH=arm
 export EXTRADIR=${PWD}/extra
 export CROSS_COMPILE=arm-none-linux-gnueabi-
 make prima2cb_defconfig
-make uImage KALLSYMS_EXTRA_PASS=1
-make modules
+make uImage KALLSYMS_EXTRA_PASS=1 -j8
+make modules -j8
 make dtbs
 cp arch/arm/boot/uImage extra
 cp arch/arm/boot/prima2-cb.dtb	extra/dtb
