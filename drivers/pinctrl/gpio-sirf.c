1296e1ed1054da76049cb638c4b7ed49b99692ea
Barry Song <Barry.Song@csr.com>
2012-05-21 14:59:42 +0800

    pinctrl: sirf: gpio: delete redundant head files
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- drivers/pinctrl/gpio-sirf.c
+++ drivers/pinctrl/gpio-sirf.c
@@ -18,15 +18,30 @@
 #include <linux/pinctrl/pinctrl.h>
 #include <linux/pinctrl/consumer.h>
 
-#include <mach/hardware.h>
-#include <mach/irqs.h>
-#include <mach/regs-gpio.h>
-
 #define SIRFSOC_IRQ_SIRFSOC_GPIO_GROUP0         43
 #define SIRFSOC_IRQ_SIRFSOC_GPIO_GROUP1         44
 #define SIRFSOC_IRQ_SIRFSOC_GPIO_GROUP2         45
 #define SIRFSOC_IRQ_SIRFSOC_GPIO_GROUP3         46
 #define SIRFSOC_IRQ_SIRFSOC_GPIO_GROUP4         47
+
+#define SIRFSOC_GPIO_CTRL(g, i)			((g)*0x100 + (i)*4)
+#define SIRFSOC_GPIO_DSP_EN0			(0x80)
+#define SIRFSOC_GPIO_PAD_EN(g)			((g)*0x100 + 0x84)
+#define SIRFSOC_GPIO_INT_STATUS(g)		((g)*0x100 + 0x8C)
+
+#define SIRFSOC_GPIO_CTL_INTR_LOW_MASK		0x1
+#define SIRFSOC_GPIO_CTL_INTR_HIGH_MASK		0x2
+#define SIRFSOC_GPIO_CTL_INTR_TYPE_MASK		0x4
+#define SIRFSOC_GPIO_CTL_INTR_EN_MASK		0x8
+#define SIRFSOC_GPIO_CTL_INTR_STS_MASK		0x10
+#define SIRFSOC_GPIO_CTL_OUT_EN_MASK		0x20
+#define SIRFSOC_GPIO_CTL_DATAOUT_MASK		0x40
+#define SIRFSOC_GPIO_CTL_DATAIN_MASK		0x80
+#define SIRFSOC_GPIO_CTL_PULL_MASK		0x100
+#define SIRFSOC_GPIO_CTL_PULL_HIGH		0x200
+#define SIRFSOC_GPIO_CTL_DSP_INT		0x400
+
+#define SIRFSOC_GPIO_NUM(bank, index)	(((bank)*(32)) + (index))
 
 struct sirfsoc_gpio_bank {
 	struct of_mm_gpio_chip chip;
