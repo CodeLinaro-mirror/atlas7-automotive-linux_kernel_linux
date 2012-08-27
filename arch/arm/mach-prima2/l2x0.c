2078450c865face3cafde09f753712103846fb70
Barry Song <Barry.Song@csr.com>
2012-08-27 18:03:38 +0800

    ARM: MARCO: split l2x0 from prima2 since they are diffirent
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- arch/arm/mach-prima2/l2x0.c
+++ arch/arm/mach-prima2/l2x0.c
@@ -11,8 +11,16 @@
 #include <linux/of.h>
 #include <asm/hardware/cache-l2x0.h>
 
+/*
+ * fixme: make aux_val and aux_mask the private data
+ * of of_device_id after we get right aux_val/mask
+ */
 static struct of_device_id prima2_l2x0_ids[]  = {
 	{ .compatible = "sirf,prima2-pl310-cache" },
+	{},
+};
+
+static struct of_device_id marco_l2x0_ids[]  = {
 	{ .compatible = "sirf,marco-pl310-cache" },
 	{},
 };
@@ -20,11 +28,19 @@ static struct of_device_id prima2_l2x0_ids[]  = {
 static int __init sirfsoc_l2x0_init(void)
 {
 	struct device_node *np;
-
 	np = of_find_matching_node(NULL, prima2_l2x0_ids);
 	if (np) {
 		pr_info("Initializing prima2 L2 cache\n");
 		return l2x0_of_init(0x40000, 0);
+	}
+
+	np = of_find_matching_node(NULL, marco_l2x0_ids);
+	if (np) {
+		pr_info("Initializing marco L2 cache\n");
+		/*
+		 * fixme: set the right aux_val and aux_mask
+		 * return l2x0_of_init(0x10000, 0);
+		 */
 	}
 
 	return 0;
