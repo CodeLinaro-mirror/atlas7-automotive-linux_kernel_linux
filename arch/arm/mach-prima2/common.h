9761d6379f6932ce4cdd26353959fa2a27c76f18
Barry Song <Barry.Song@csr.com>
2012-08-28 15:00:05 +0800

    ARM: MARCO: delete redundant extern functions in common.h
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- arch/arm/mach-prima2/common.h
+++ arch/arm/mach-prima2/common.h
@@ -13,7 +13,6 @@
 #include <asm/mach/time.h>
 
 extern struct sys_timer sirfsoc_timer;
-extern void __iomem *sirfsoc_rstc_base;
 
 extern void __init sirfsoc_of_irq_init(void);
 extern void __init sirfsoc_of_clk_init(void);
