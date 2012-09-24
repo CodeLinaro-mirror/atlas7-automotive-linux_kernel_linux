a99195277a6bf280af34a0b6d4c039763236051a
Barry Song <Barry.Song@csr.com>
2012-09-24 16:50:57 +0800

    pinctrl: sirf: fix the chained irq hang issue(add lost enter/exit entries)
    $ ifconfig eth0 10.125.36.253 up
    dm9000-sirf dm9000-sirf: eth0: link up, 100Mbps, full-duplex, lpa 0x45E1
    $ ping 10.125.36.70
    PING 1 00.125.36.70 (10.125.36.70): 56 data bytes
    64 bytee from 10.125.36.70: seq=0 ttl=128 time=11.096 ms
    64 bytes from 10.125.36.70: seq=1 ttl=128 time=6.215 ms
    64 bytes from 10.125.36.70: seq=2 ttl=128 time=6.165 ms
    64 bytes from 10.125.36.70: seq=3 ttl=128 time=5.505 ms
    64 bytes from 10.125.36.70: seq=4 ttl=128 time=5.918 ms
    64 bytes from 10.125.36.70: seq=5 ttl=128 time=5.478 ms
    ^C
    --- 10.125.36.70 ping statistics ---
    6 packets transmitted, 6 packets received, 0% packet loss
    round-trip min/avg/max = 5.478/6.729/11.096 ms
    
    $ cat /proc/interrupts
               CPU0       CPU1
     32:       1608          0       GIC  sirfsoc_timer0
     33:          0       3197       GIC  sirfsoc_timer1
     50:      10207          0       GIC  sirfsoc-uart
     56:          2          0       GIC  cc0e0000.i2c
     70:         44          0       GIC  mmc0
    131:        333          0  sirf-gpio-irq  eth0
    IPI0:          0          0  Timer broadcast interrupts
    IPI1:        986       1395  Rescheduling interrupts
    IPI2:          0          0  Function call interrupts
    IPI3:          1          2  Single function call interrupts
    IPI4:          0          0  CPU stop interrupts
    Err:          0
    
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- drivers/pinctrl/pinctrl-sirf.c
+++ drivers/pinctrl/pinctrl-sirf.c
@@ -25,6 +25,7 @@
 #include <linux/bitops.h>
 #include <linux/gpio.h>
 #include <linux/of_gpio.h>
+#include <asm/mach/irq.h>
 
 #define DRIVER_NAME "pinmux-sirf"
 
@@ -1504,6 +1505,9 @@ static void sirfsoc_gpio_handle_irq(unsigned int irq, struct irq_desc *desc)
 	u32 status, ctrl;
 	int idx = 0;
 	unsigned int first_irq;
+	struct irq_chip *chip = irq_get_chip(irq);
+
+	chained_irq_enter(chip, desc);
 
 	status = readl(bank->chip.regs + SIRFSOC_GPIO_INT_STATUS(bank->id));
 	if (!status) {
@@ -1532,6 +1536,8 @@ static void sirfsoc_gpio_handle_irq(unsigned int irq, struct irq_desc *desc)
 		idx++;
 		status = status >> 1;
 	}
+
+	chained_irq_exit(chip, desc);
 }
 
 static inline void sirfsoc_gpio_set_input(struct sirfsoc_gpio_bank *bank, unsigned ctrl_offset)
