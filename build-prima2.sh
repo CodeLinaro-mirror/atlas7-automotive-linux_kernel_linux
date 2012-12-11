d7e921262aa84cb9548630ea7ced716efd3dc95e
Barry Song <Barry.Song@csr.com>
2012-12-11 14:32:45 +0800

    build-prima2: use zImage instead of uImage
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- build-prima2.sh
+++ build-prima2.sh
@@ -2,9 +2,9 @@ export ARCH=arm
 export EXTRADIR=${PWD}/extra
 export CROSS_COMPILE=arm-none-linux-gnueabi-
 make prima2cb_defconfig
-make uImage KALLSYMS_EXTRA_PASS=1 -j8
+make zImage KALLSYMS_EXTRA_PASS=1 -j8
 make modules -j8
 make dtbs V=1
-cp arch/arm/boot/uImage extra
+cp arch/arm/boot/zImage extra
 cp arch/arm/boot/prima2-evb.dtb	extra/dtb
 cp .config	extra/
