11438d4a5fac2c4bb0c1fe973ed332a150dc17d1
Barry Song <Barry.Song@csr.com>
2012-12-21 11:06:24 +0800

    arm: prima2: enable provide handle_irq when CONFIG_MULTI_IRQ_HANDLER is enabled
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- arch/arm/mach-prima2/common.c
+++ arch/arm/mach-prima2/common.c
@@ -48,7 +48,9 @@ DT_MACHINE_START(PRIMA2_DT, "Generic PRIMA2 (Flattened Device Tree)")
 	/* Maintainer: Barry Song <baohua.song@csr.com> */
 	.map_io         = sirfsoc_map_io,
 	.init_irq	= sirfsoc_of_irq_init,
+#ifdef CONFIG_MULTI_IRQ_HANDLER
 	.handle_irq	= sirfsoc_handle_irq,
+#endif
 	.timer		= &sirfsoc_prima2_timer,
 	.dma_zone_size	= SZ_256M,
 	.init_machine	= sirfsoc_mach_init,
