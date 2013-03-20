95ce3675ea84ff14e74ee1ca886fdcacbd4c5141
Barry Song <Barry.Song@csr.com>
2013-03-20 11:12:19 +0800

    sdhci:sirf:privatize platformdata and add gpio detection support
    Change-Id: I46162bbbfb7168cb92f1d1da6a4baa01cc636415
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- drivers/mmc/host/sdhci-sirf.c
+++ drivers/mmc/host/sdhci-sirf.c
@@ -11,27 +11,43 @@
 #include <linux/mmc/host.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/of_gpio.h>
 #include "sdhci-pltfm.h"
 
-/* Fixme: make it the platform data of sdhci_host */
+struct sdhci_sirf_priv {
+	struct clk *clk;
+	u32 clock;
+	int gpio_cd;
+};
 
-static u32 sdhci_sirf_clkrate = 26000000; /* For FPGA */
-struct clk *sdhci_sirf_clk;
+static irqreturn_t sdhci_sirf_carddetect_irq(int irq, void *data)
+{
+	struct sdhci_host *host = data;
+
+	tasklet_schedule(&host->card_tasklet);
+	return IRQ_HANDLED;
+}
 
 static unsigned int sdhci_sirf_get_max_clk(struct sdhci_host *host)
 {
-	return sdhci_sirf_clkrate;
+	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
+	struct sdhci_sirf_priv *priv = pltfm_host->priv;
+	return priv->clock;
 }
 
 #ifdef CONFIG_PM
 static void sdhci_sirf_suspend(struct sdhci_host *host)
 {
-	clk_disable_unprepare(sdhci_sirf_clk);
+	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
+	struct sdhci_sirf_priv *priv = pltfm_host->priv;
+	clk_disable_unprepare(priv->clk);
 }
 
 static void sdhci_sirf_resume(struct sdhci_host *host)
 {
-	clk_prepare_enable(sdhci_sirf_clk);
+	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
+	struct sdhci_sirf_priv *priv = pltfm_host->priv;
+	clk_prepare_enable(priv->clk);
 }
 #endif
 
@@ -55,24 +71,104 @@ static struct sdhci_pltfm_data sdhci_sirf_pdata = {
 
 static int sdhci_sirf_probe(struct platform_device *pdev)
 {
-	/* 
-	 * we use 26MB for all marco for the moment, and get mmc clk
-	 * from DT for primaII
-	 */
-	if (of_device_is_compatible(pdev->dev.of_node, "sirf,prima2-sdhc")) {
-		sdhci_sirf_clk = clk_get(&pdev->dev, NULL);
-		clk_prepare_enable(sdhci_sirf_clk);
-		sdhci_sirf_clkrate = clk_get_rate(sdhci_sirf_clk);
+	struct sdhci_host *host;
+	struct sdhci_pltfm_host *pltfm_host;
+	struct sdhci_sirf_priv *priv;
+	int ret;
+
+	priv = devm_kzalloc(&pdev->dev, sizeof(struct sdhci_sirf_priv),
+		GFP_KERNEL);
+	if (!priv) {
+		dev_err(&pdev->dev, "unable to allocate private data");
+		return -ENOMEM;
 	}
 
-	return sdhci_pltfm_register(pdev, &sdhci_sirf_pdata);
+	priv->clk = devm_clk_get(&pdev->dev, NULL);
+	if (IS_ERR(priv->clk)) {
+		dev_err(&pdev->dev, "unable to get clock");
+		return PTR_ERR(priv->clk);
+	}
+
+	if (pdev->dev.of_node) {
+		priv->gpio_cd = of_get_named_gpio(pdev->dev.of_node,
+			"cd-gpios", 0);
+	} else {
+		priv->gpio_cd = -EINVAL;
+	}
+
+	if (gpio_is_valid(priv->gpio_cd)) {
+		ret = gpio_request(priv->gpio_cd, "sdhci-cd");
+		if (ret) {
+			dev_err(&pdev->dev, "card detect gpio request failed: %d\n",
+				ret);
+			return ret;
+		}
+		gpio_direction_input(priv->gpio_cd);
+	}
+
+	host = sdhci_pltfm_init(pdev, &sdhci_sirf_pdata);
+	if (IS_ERR(host)) {
+		ret = PTR_ERR(host);
+		goto err_sdhci_pltfm_init;
+	}
+
+	pltfm_host = sdhci_priv(host);
+	pltfm_host->priv = priv;
+
+	sdhci_get_of_property(pdev);
+
+	clk_prepare_enable(priv->clk);
+	priv->clock = clk_get_rate(priv->clk);
+
+	ret = sdhci_add_host(host);
+	if (ret)
+		goto err_sdhci_add;
+
+	/*
+	 * We must request the IRQ after sdhci_add_host(), as the tasklet only
+	 * gets setup in sdhci_add_host() and we oops.
+	 */
+	if (gpio_is_valid(priv->gpio_cd)) {
+		ret = request_irq(gpio_to_irq(priv->gpio_cd),
+			sdhci_sirf_carddetect_irq,
+			IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING,
+			mmc_hostname(host->mmc), host);
+		if (ret) {
+			dev_err(&pdev->dev, "card detect irq request failed: %d\n",
+				ret);
+			goto err_request_irq;
+		}
+	}
+
+	return 0;
+
+err_request_irq:
+	sdhci_remove_host(host, 0);
+err_sdhci_add:
+	clk_disable_unprepare(priv->clk);
+	sdhci_pltfm_free(pdev);
+err_sdhci_pltfm_init:
+	if (gpio_is_valid(priv->gpio_cd))
+		gpio_free(priv->gpio_cd);
+	return ret;
+
 }
 
 static int sdhci_sirf_remove(struct platform_device *pdev)
 {
-	clk_disable_unprepare(sdhci_sirf_clk);
-	clk_put(sdhci_sirf_clk);
-	return sdhci_pltfm_unregister(pdev);
+	struct sdhci_host *host = platform_get_drvdata(pdev);
+	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
+	struct sdhci_sirf_priv *priv = pltfm_host->priv;
+
+	sdhci_pltfm_unregister(pdev);
+
+	if (gpio_is_valid(priv->gpio_cd)) {
+		free_irq(gpio_to_irq(priv->gpio_cd), host);
+		gpio_free(priv->gpio_cd);
+	}
+
+	clk_disable_unprepare(priv->clk);
+	return 0;
 }
 
 static const struct of_device_id sdhci_sirf_of_match[] = {
