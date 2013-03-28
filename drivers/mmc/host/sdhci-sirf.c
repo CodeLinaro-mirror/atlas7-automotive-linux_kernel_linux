7441b25e7c3a4889ea7e0beb6b5953aa36e3bd99
Barry Song <Barry.Song@csr.com>
2013-03-28 13:38:46 +0800

    sdhci: sirf: delete the redundant get_default_pinctrl
    This is done by the device core as of commit ab78029ecc347debbd737f06688d788bd9d60c1d
    "drivers/pinctrl: grab default handles from device core".
    
    You only need to fetch pinctrl handles in drivers if you're using anything
    else than the default state.
    
    Signed-off-by: Barry Song <Barry.Song@csr.com>

--- drivers/mmc/host/sdhci-sirf.c
+++ drivers/mmc/host/sdhci-sirf.c
@@ -46,14 +46,7 @@ static int sdhci_sirf_probe(struct platform_device *pdev)
 	struct sdhci_host *host;
 	struct sdhci_pltfm_host *pltfm_host;
 	struct sdhci_sirf_priv *priv;
-	struct pinctrl *pinctrl;
 	int ret;
-
-	pinctrl = devm_pinctrl_get_select_default(&pdev->dev);
-	if (IS_ERR(pinctrl)) {
-		dev_err(&pdev->dev, "unable to get pinmux");
-		return PTR_ERR(pinctrl);
-	}
 
 	priv = devm_kzalloc(&pdev->dev, sizeof(struct sdhci_sirf_priv),
 		GFP_KERNEL);
