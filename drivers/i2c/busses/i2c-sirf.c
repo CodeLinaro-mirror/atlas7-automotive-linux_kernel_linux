e134a91f6a616b58818599e57e2bfdc044a03267
Barry Song <Baohua.Song@csr.com>
2013-05-16 10:38:24 +0800

    i2c: sirf: Let device core handle pinctrl
    Since commit ab78029 (drivers/pinctrl: grab default handles from device
    core) we can rely on device core for handling pinctrl so remove
    devm_pinctrl_get_select_default() from the driver.
    
    Change-Id: I4f5a14f8517bb0df33638b21b03345408ec32f0c
    Signed-off-by: Barry Song <Baohua.Song@csr.com>

--- drivers/i2c/busses/i2c-sirf.c
+++ drivers/i2c/busses/i2c-sirf.c
@@ -16,7 +16,6 @@
 #include <linux/clk.h>
 #include <linux/err.h>
 #include <linux/io.h>
-#include <linux/pinctrl/consumer.h>
 
 #define SIRFSOC_I2C_CLK_CTRL		0x00
 #define SIRFSOC_I2C_STATUS		0x0C
@@ -266,19 +265,12 @@ static int i2c_sirfsoc_probe(struct platform_device *pdev)
 	struct i2c_adapter *adap;
 	struct resource *mem_res;
 	struct clk *clk;
-	struct pinctrl *pinctrl;
 	int bitrate;
 	int ctrl_speed;
 	int irq;
 
 	int err;
 	u32 regval;
-
-	pinctrl = devm_pinctrl_get_select_default(&pdev->dev);
-	if (IS_ERR(pinctrl)) {
-		err = PTR_ERR(pinctrl);
-		goto failed_pin;
-	}
 
 	clk = clk_get(&pdev->dev, NULL);
 	if (IS_ERR(clk)) {
@@ -393,7 +385,6 @@ err_clk_en:
 err_clk_prep:
 	clk_put(clk);
 err_get_clk:
-failed_pin:
 	return err;
 }
 
