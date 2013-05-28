1b60719221c0475519a39d11c270e59bc0cfba97
Barry Song <Baohua.Song@csr.com>
2013-05-28 10:26:37 +0800

    input: sirfsoc-onkey: a bundle of cleanup for upstream
    Change-Id: Ie2382e3b5a487c0cf3b2b98114a91f94546ce892
    Signed-off-by: Barry Song <Baohua.Song@csr.com>
    Signed-off-by: Xianglong Du <Xianglong.Du@csr.com>

--- drivers/input/misc/sirfsoc-onkey.c
+++ drivers/input/misc/sirfsoc-onkey.c
@@ -8,27 +8,15 @@
 
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/fs.h>
 #include <linux/interrupt.h>
-#include <linux/irq.h>
-#include <linux/sched.h>
-#include <linux/pm.h>
-#include <linux/sysctl.h>
-#include <linux/proc_fs.h>
 #include <linux/delay.h>
 #include <linux/platform_device.h>
 #include <linux/input.h>
-#include <linux/apm-emulation.h>
 #include <linux/rtc/sirfsoc_rtciobrg.h>
-#include <linux/suspend.h>
-#include <linux/reboot.h>
-#include <linux/syscalls.h>
 #include <linux/of.h>
-#include <linux/io.h>
 
 struct sirfsoc_pwrc_drvdata {
 	u32			pwrc_base;
-	int			irq;
 	struct input_dev	*input;
 };
 
@@ -46,8 +34,19 @@ static irqreturn_t sirfsoc_pwrc_isr(int irq, void *dev_id)
 			pwrcdrv->pwrc_base + PWRC_INT_STATUS);
 	sirfsoc_rtc_iobrg_writel(int_status & (~PWRC_ON_KEY_BIT),
 			pwrcdrv->pwrc_base + PWRC_INT_STATUS);
+
+	/*
+	 * For a typical Linux system, we report KEY_SUSPEND to trigger apm-power.c
+	 * to queue a SUSPEND APM event
+	 */
 	input_event(pwrcdrv->input, EV_PWR, KEY_SUSPEND, 1);
 	input_sync(pwrcdrv->input);
+
+	/*
+	 * Todo: report KEY_POWER event for Android platforms, Android PowerManager
+	 * will handle the suspend and powerdown/hibernation
+	 */
+
 	return IRQ_HANDLED;
 }
 
@@ -57,13 +56,9 @@ static const struct of_device_id sirfsoc_pwrc_of_match[] = {
 }
 MODULE_DEVICE_TABLE(of, sirfsoc_pwrc_of_match);
 
-
-/*
- * power keys include ON_KEY and EXT_ON
- */
 static int sirfsoc_pwrc_probe(struct platform_device *pdev)
 {
-	int ret;
+	int ret, irq;
 	struct sirfsoc_pwrc_drvdata *pwrcdrv = NULL;
 	struct device_node *np = pdev->dev.of_node;
 
@@ -74,6 +69,10 @@ static int sirfsoc_pwrc_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
+	/*
+	 * we can't use of_iomap because pwrc is not mapped in memory, the so-called base
+	 * address is only offset in rtciobrg
+	 */
 	ret = of_property_read_u32(np, "reg", &pwrcdrv->pwrc_base);
 	if (ret) {
 		dev_err(&pdev->dev, "unable to find base address of pwrc node in dtb\n");
@@ -89,20 +88,22 @@ static int sirfsoc_pwrc_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, pwrcdrv);
 
-	pwrcdrv->irq = platform_get_irq(pdev, 0);
-	ret = devm_request_irq(&pdev->dev, pwrcdrv->irq,
+	irq = platform_get_irq(pdev, 0);
+	ret = devm_request_irq(&pdev->dev, irq,
 			sirfsoc_pwrc_isr, IRQF_SHARED,
 			"sirfsoc_pwrc_int", pwrcdrv);
+	if (ret) {
+		dev_err(&pdev->dev, "pwrc: Unable to claim irq %d; error %d\n",
+			irq, ret);
+		return ret;
+	}
 
 	sirfsoc_rtc_iobrg_writel(
 		sirfsoc_rtc_iobrg_readl(pwrcdrv->pwrc_base + PWRC_INT_MASK)
 		| PWRC_ON_KEY_BIT, pwrcdrv->pwrc_base + PWRC_INT_MASK);
-	if (ret) {
-		dev_err(&pdev->dev, "pwrc: Unable to claim irq %d; error %d\n",
-			pwrcdrv->irq, ret);
-		return ret;
-	}
-	pwrcdrv->input->evbit[0] = BIT_MASK(EV_PWR);
+
+	pwrcdrv->input->evbit[0] = BIT_MASK(EV_PWR) | BIT_MASK(EV_KEY);
+	set_bit(KEY_POWER, pwrcdrv->input->keybit);
 
 	ret = input_register_device(pwrcdrv->input);
 	if (ret) {
@@ -124,16 +125,14 @@ static int sirfsoc_pwrc_remove(struct platform_device *pdev)
 	return 0;
 }
 
-#ifdef CONFIG_PM
-static int pwrc_suspend(struct device *dev)
-{
-	return 0;
-}
-
+#ifdef CONFIG_PM_SLEEP
 static int pwrc_resume(struct device *dev)
 {
 	struct sirfsoc_pwrc_drvdata *pwrcdrv = dev_get_drvdata(dev);
-	/* Do not mask pwrcdrv->irq interrupt */
+	/*
+	 * Do not mask pwrc interrupt as we want pwrc work as a wakeup source
+	 * if users touch X_ONKEY_B, see arch/arm/mach-prima2/pm.c
+	 */
 	sirfsoc_rtc_iobrg_writel(
 		sirfsoc_rtc_iobrg_readl(
 		pwrcdrv->pwrc_base + PWRC_INT_MASK) | PWRC_ON_KEY_BIT,
@@ -141,12 +140,9 @@ static int pwrc_resume(struct device *dev)
 
 	return 0;
 }
-
-static const struct dev_pm_ops sirfsoc_pwrc_pm_ops = {
-	.suspend = pwrc_suspend,
-	.resume	= pwrc_resume,
-};
 #endif
+
+static SIMPLE_DEV_PM_OPS(sirfsoc_pwrc_pm_ops, NULL, pwrc_resume);
 
 static struct platform_driver sirfsoc_pwrc_driver = {
 	.probe		= sirfsoc_pwrc_probe,
@@ -154,9 +150,7 @@ static struct platform_driver sirfsoc_pwrc_driver = {
 	.driver		= {
 		.name	= "sirfsoc-pwrc",
 		.owner	= THIS_MODULE,
-#ifdef CONFIG_PM
 		.pm	= &sirfsoc_pwrc_pm_ops,
-#endif
 		.of_match_table = of_match_ptr(sirfsoc_pwrc_of_match),
 	}
 };
@@ -164,7 +158,6 @@ static struct platform_driver sirfsoc_pwrc_driver = {
 module_platform_driver(sirfsoc_pwrc_driver);
 
 MODULE_LICENSE("GPLv2");
-MODULE_AUTHOR("Binghua Duan <Binghua.Duan@csr.com>, "
-		"Xianglong Du <Xianglong.Du@csr.com>");
+MODULE_AUTHOR("Binghua Duan <Binghua.Duan@csr.com>, Xianglong Du <Xianglong.Du@csr.com>");
 MODULE_DESCRIPTION("CSR Prima2 PWRC Driver");
 MODULE_ALIAS("platform:sirfsoc-pwrc");
