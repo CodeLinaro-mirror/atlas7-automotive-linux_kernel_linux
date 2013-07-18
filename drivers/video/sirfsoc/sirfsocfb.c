3c467e0da76006db3159472833ac3c501c200f98
Renwei Wu <renwei.wu@csr.com>
2013-07-18 10:35:47 +0800

    fb:sirf: remove backlight control from framebuffer driver As pwm driver already works in system, framebuffer driver will not control backlight anymore.
    Change-Id: I49aa34819a5fca427248e6165ce66d8accda0ead
    Signed-off-by: Renwei Wu <renwei.wu@csr.com>

--- drivers/video/sirfsoc/sirfsocfb.c
+++ drivers/video/sirfsoc/sirfsocfb.c
@@ -1820,7 +1820,6 @@ static void sirfsocfb_probe_async(void *async_data, async_cookie_t cookie)
 	struct pinctrl *p;
 	int i, ret = 0;
 	int    layer_ctrl;
-	int bl_gpio;
 	LCD_PANEL_INFO panel_info;
 
 	FB_FUN_MSG("+sirfsocfb_probe\n");
@@ -1946,18 +1945,6 @@ static void sirfsocfb_probe_async(void *async_data, async_cookie_t cookie)
 	}
 
 	get_layer_ctrl_info(fb, layer_ctrl);
-
-	/* later bl should be managed by pwm */
-	bl_gpio = of_get_named_gpio(pdev->dev.of_node, "bl-gpios", 0);
-	if (gpio_is_valid(bl_gpio)) {
-		ret = devm_gpio_request(&pdev->dev, bl_gpio, "sirfsoc_backlight");
-		if (ret) {
-			dev_err(&pdev->dev, "request backlight gpio failed\n");
-			ret = -ENODEV;
-			goto err_unmap;
-		}
-		gpio_direction_output(bl_gpio, 1);
-	}
 
 	vcc_gpio = of_get_named_gpio(pdev->dev.of_node, "vcc-gpios", 0);
 	if (gpio_is_valid(vcc_gpio)) {
