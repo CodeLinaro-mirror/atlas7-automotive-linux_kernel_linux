adb4dd76bca2be1dc2bb7f387662e237ceb53170
Rongjun Ying <rongjun.ying@csr.com>
2013-09-11 10:25:16 +0800

    ASoC: sirf i2s: Move the channel configation setting into hw_params function
    Change-Id: Iffcb52d5385092f3d78f385a7275ce38450788eb
    Signed-off-by: Rongjun Ying <rongjun.ying@csr.com>

--- sound/soc/sirf/sirf-i2s.c
+++ sound/soc/sirf/sirf-i2s.c
@@ -119,29 +119,30 @@ static int sirf_i2s_trigger(struct snd_pcm_substream *substream,
 	return 0;
 }
 
-static int sirf_i2s_prepare(struct snd_pcm_substream *substream,
-			struct snd_soc_dai *dai)
-{
-	struct snd_pcm_runtime *runtime = substream->runtime;
-	struct sirf_i2s *si2s = snd_soc_dai_get_drvdata(dai);
-	u32 ctrl = readl(si2s->base + AUDIO_CTRL_I2S_CTRL);
-
-	if (runtime->channels == 2)
-		ctrl &= ~I2S_SIX_CHANNELS;
-	else
-		ctrl |= I2S_SIX_CHANNELS;
-
-	writel(ctrl, si2s->base+AUDIO_CTRL_I2S_CTRL);
-
-	return 0;
-}
-
 static int sirf_i2s_hw_params(struct snd_pcm_substream *substream,
 		struct snd_pcm_hw_params *params, struct snd_soc_dai *dai)
 {
 	struct sirf_i2s *si2s = snd_soc_dai_get_drvdata(dai);
 	u32 i2s_ctrl = readl(si2s->base + AUDIO_CTRL_I2S_CTRL);
 	u32 left_len, frame_len;
+	int channels = params_channels(params);
+
+	/*
+	 * SiRFSoC I2S controller only support 2 and 6 channells output.
+	 * I2S_SIX_CHANNELS bit clear: select 2 channels mode.
+	 * I2S_SIX_CHANNELS bit set: select 6 channels mode.
+	 */
+	switch (channels) {
+	case 2:
+		i2s_ctrl &= ~I2S_SIX_CHANNELS;
+		break;
+	case 6:
+		i2s_ctrl |= I2S_SIX_CHANNELS;
+		break;
+	default:
+		dev_err(dai->dev, "%d channels unsupported\n", channels);
+		return -EINVAL;
+	}
 
 	switch (params_format(params)) {
 	case SNDRV_PCM_FORMAT_S8:
@@ -218,7 +219,6 @@ struct snd_soc_dai_ops sirfsoc_i2s_dai_ops = {
 	.startup	= sirf_i2s_startup,
 	.shutdown	= sirf_i2s_shutdown,
 	.trigger	= sirf_i2s_trigger,
-	.prepare	= sirf_i2s_prepare,
 	.hw_params	= sirf_i2s_hw_params,
 	.set_fmt	= sirf_i2s_set_dai_fmt,
 };
