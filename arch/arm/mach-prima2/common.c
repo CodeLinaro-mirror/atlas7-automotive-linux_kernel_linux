f7546c2909e7acf3b73f0225203a697c3ff91fc3
Qipan Li <Qipan.Li@csr.com>
2013-10-17 19:12:36 +0800

    ARM: sirf: put nanddisk reserve memory first
    fix bug APBU-7725.
    nanddisk using 'memblock_reserve' which allow reserve memory overlaped
    with others; other system modules using 'memblock_alloc' dynamically
    reserve memory will not touch existed reserved memory.
    
    in older case system reserve memory may simply looks like this:
    	128MB   +++++++++
    	     +  sys reserve  +
    		+++++++++        ---------------------------
    		+  fb   +       -                          -
    		+++++++++------   nanddisk resever memory  -
    		+  vip  +       - (overlap with others)    -
    	<91MB   +++++++++        ---------------------------
    		+       +
    		+++++++++
    which may conflict with other modules while nanddisk reserve memory at
    address 0x6000000.
    
    this patch put nanddisk reserve works first will avoid it.
    Change-Id: I493a5db60cf49dffe6be8d628cf5e8f78650f526
    Signed-off-by: Qipan Li <Qipan.Li@csr.com>

--- arch/arm/mach-prima2/common.c
+++ arch/arm/mach-prima2/common.c
@@ -19,9 +19,9 @@
 
 void __init sirfsoc_reserve(void)
 {
+	sirfsoc_nand_reserve_memblock();
 	sirfsoc_fb_reserve_memblock();
 	sirfsoc_vip_reserve_memblock();
-	sirfsoc_nand_reserve_memblock();
 	if (of_machine_is_compatible("sirf,prima2"))
 		sirfsoc_video_codec_reserve_memblock();
 }
