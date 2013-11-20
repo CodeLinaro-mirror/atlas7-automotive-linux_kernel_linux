89c6667a5b3ee9ba33bad7fd340e53b79388b8e3
Bin Shi <Bin.Shi@csr.com>
2013-11-20 17:46:32 +0800

    mmc : sirf : involve set_power_config() for hacked ctller
    Since some SD/MMC controller such as SiRFSocs' be hacked in power definition in
    register offset 0x29, bit [0-3]
    
    Standard SDHCI spec says:
    SDHCI_POWER_ON	0x01 -> bit[0,0]
    SDHCI_POWER_180	0x0A -> bit[1,2]
    SDHCI_POWER_300	0x0C -> bit[1,2]
    SDHCI_POWER_330	0x0E -> bit[1,2]
    
    While SiRFprimaII spec says:
    SDHCI_POWER_ON	0x01 -> bit[0,0]
    SDHCI_POWER_180	0x01 -> bit[1,1]
    SDHCI_POWER_330	0x00 -> bit[1,1]
    
    So for use of standard SDHCI driver in Linux kernel, one callback get_power_config()
    in sdhci_host->ops has to be defined, and each platform could achieve themselve
    in seperate driver.
    
    Change-Id: I16a3cf54b6cb66de45024cd25607ebe4c857e440
    Signed-off-by: Bin Shi <Bin.Shi@csr.com>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -294,6 +294,7 @@ struct sdhci_ops {
 	void	(*platform_resume)(struct sdhci_host *host);
 	void    (*adma_workaround)(struct sdhci_host *host, u32 intmask);
 	void	(*platform_init)(struct sdhci_host *host);
+	unsigned int	(*get_power_config)(struct sdhci_host *host, unsigned short power);
 };
 
 #ifdef CONFIG_MMC_SDHCI_IO_ACCESSORS
