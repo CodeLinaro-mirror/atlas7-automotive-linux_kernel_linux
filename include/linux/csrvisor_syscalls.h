44d08ffcda3c30aff0ad217d2f0f3a0432713c46
Wei Chen <Wei.Chen@csr.com>
2014-01-15 13:35:48 +0800

    include:linux:add a header file of csrvisor syscalls
    CSR hypervisor had provided some features to backend and frontend OS.
    These features had been encapsulated as syscalls. We added this header
    file for two OS to do hypervisor call.
    
    Change-Id: Ieffc02b446295aa67192c5a924b19e3c1e605667
    Signed-off-by: Wei Chen <Wei.Chen@csr.com>

---
+++ include/linux/csrvisor_syscalls.h
@@ -0,0 +1,171 @@
+
+/*
+ * CSR hypervisor-call interfaces.
+ *
+ * Copyright (c) 2013 Cambridge Silicon Radio Limited, a CSR plc group company.
+ *
+ * Licensed under GPLv2 or later.
+ */
+#ifndef _LINUX_CSRVISOR_SYSCALLS_H_
+#define _LINUX_CSRVISOR_SYSCALLS_H_
+
+/*
+ * CSRVISOR System Monitor Calls
+ */
+#define T_SMC_SWITCH  0		/* switch to NT */
+#define	 T_SMC_SWITCH_OK	  0	/* no error */
+#define	 T_SMC_SWITCH_ERR	 1	/* error, return to T */
+#define	 T_SMC_SWITCH_HANDLER 2	/* NT returns, raise csrvisor handler */
+#define	 T_SMC_SWITCH_RET	 3	/* NT returns */
+#define	 T_SMC_SWITCH_SHVAR   4	/* NT returns, arg1 = shared variable */
+#define	 T_SMC_SWITCH_VERIFY  5	/* obj_csrvisor_monitor */
+
+#define T_SMC_REBOOT  1		/* reboot NT */
+#define	 T_SMC_REBOOT_OK	  0	/* no error */
+#define	 T_SMC_REBOOT_ERR	 1	/* error, return to T */
+
+#define T_SMC_PROF_INIT  2	/* initialize profile data */
+#define	 T_SMC_PROF_INIT_OK	  0	/* no error */
+#define	 T_SMC_PROF_INIT_ERR	 1	/* error, return to T */
+
+#define T_SMC_PROF_GET  3	/* get profile data */
+#define	 T_SMC_PROF_GET_OK	  0	/* no error */
+#define	 T_SMC_PROF_GET_ERR	 1	/* error, return to T */
+
+#define T_SMC_PROF_GET_PTR  4	/* get profile data pointer */
+#define	 T_SMC_PROF_GET_PTR_OK	  0	/* no error */
+#define	 T_SMC_PROF_GET_PTR_ERR	 1	/* error, return to T */
+
+#define T_SMC_FIFO_IOCTL 5  /* fifo ioctl */
+#define T_SMC_SET_ISPR 0x10 /* set interrupt set-pending register address */
+
+#define NT_SMC_SWITCH 0		/* switch to T */
+#define	 NT_SMC_SWITCH_OK	 0	/* no error */
+#define	 NT_SMC_SWITCH_ERR	1	/* error, return to NT */
+
+#define NT_SMC_FIFO_IOCTL 3	/* fifo ioctl*/
+#define NT_SMC_STEAL_GET 0x10   /* get stolen time */
+
+
+#ifndef __ASSEMBLY__
+
+struct csrvisor_fifo_ioctl;
+
+#ifdef CONFIG_SECURITY_MODE
+
+struct csrvisor_smc_args {
+	void *arg0;
+	void *arg1;
+	void *arg2;
+	void *arg3;
+	void *arg4;
+};
+
+/* syscall interfaces to secure OS */
+static inline void csrvisor_switch_to_nt(volatile struct csrvisor_smc_args
+	*ret_args)
+{
+	ret_args->arg0 = (void *)T_SMC_SWITCH_OK;
+	while (1) {
+		__asm__ __volatile__(".arch_extension sec\n\t"
+		"mov r0, %0\n\t" "mov r1, %1\n\t"
+		"smc 0\n\t" :	/* no output */
+		: "I"(T_SMC_SWITCH), "r"(ret_args)
+		: "r0", "r1", "memory");
+		if (ret_args->arg0 != (void *)T_SMC_SWITCH_OK)
+			return;
+	}
+}
+
+static inline void csrvisor_set_ispr(unsigned long ispr_addr)
+{
+	register unsigned long r0 asm("r0") = T_SMC_SET_ISPR;
+	register unsigned long r1 asm("r1") = ispr_addr;
+
+	__asm__ __volatile__(".arch_extension sec\n\t"
+	"smc #0" :			/* no output */
+	: "r"(r0), "r"(r1)
+	: "memory");
+}
+
+static inline void csrvisor_fifo_ioctl(struct csrvisor_fifo_ioctl *ioctl)
+{
+	register unsigned long r0 asm("r0") = T_SMC_FIFO_IOCTL;
+	register unsigned long r1 asm("r1") = (unsigned long)ioctl;
+
+	__asm__ __volatile__(".arch_extension sec\n\t"
+	"smc #0" :			/* no output */
+	: "r"(r0), "r"(r1)
+	: "memory");
+}
+
+static inline void csrvisor_reboot_nt(void)
+{
+	__asm__ __volatile__(".arch_extension sec\n\t"
+	"mov r0, %0\n\t"
+	"smc 0\n\t" :	/* no output */
+	: "I"(T_SMC_REBOOT)
+	: "r0", "memory");
+}
+
+static inline void csrvisor_prof_init(void)
+{
+	__asm__ __volatile__(".arch_extension sec\n\t"
+	"mov r0, %0\n\t"
+	"smc 0\n\t" :	/* no output */
+	: "I"(T_SMC_PROF_INIT)
+	: "r0", "memory");
+}
+
+static inline void
+csrvisor_prof_get(uint32_t *fiq, uint32_t *irq,
+		  uint32_t *nt2t, uint32_t *t2nt)
+{
+	struct csrvisor_smc_args args;
+
+	args.arg0 = fiq;
+	args.arg1 = irq;
+	args.arg2 = nt2t;
+	args.arg3 = t2nt;
+
+	__asm__ __volatile__(".arch_extension sec\n\t"
+	"mov r0, %0\n\t" "mov r1, %1\n\t"
+	"smc 0\n\t" :	/* no output */
+	: "I"(T_SMC_PROF_GET), "r"(&args)
+	: "r0", "r1", "memory");
+}
+
+#else
+/* syscall interfaces to non-secure OS */
+static inline void csrvisor_switch_to_t(int arg0, int arg1)
+{
+	__asm__ __volatile__(".arch_extension sec\n\t"
+	"mov r0, %0\n\t" "mov r1, %1\n\t"
+	"mov r2, %2\n\t" "smc 0\n\t" :	/* no output */
+	: "I"(NT_SMC_SWITCH), "r"(arg0), "r"(arg1)
+	: "r0", "r1", "r2", "memory");
+}
+
+
+#define CP15_DCACHE_INVALIDATE_CLEAN() \
+	__asm__ __volatile__ ("mcr p15, 0, %0, c7, c14, 0" : : "r"(0))
+
+static inline void csrvisor_fifo_ioctl(struct csrvisor_fifo_ioctl *ioctl)
+{
+	register unsigned long r0 asm("r0") = NT_SMC_FIFO_IOCTL;
+	register unsigned long r1 asm("r1") = (unsigned long)ioctl;
+
+	CP15_DCACHE_INVALIDATE_CLEAN();
+
+	__asm__ __volatile__(".arch_extension sec\n\t"
+	"smc #0" :			/* no output */
+	: "r"(r0), "r"(r1)
+	: "memory");
+
+	CP15_DCACHE_INVALIDATE_CLEAN();
+}
+#endif /* CONFIG_SECURITY_MODE */
+
+#endif /* __ASSEMBLY__ */
+
+#endif /* _LINUX_CSRVISOR_SYSCALLS_H_ */
