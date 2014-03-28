bfc16ba9e1558f7e7f351be466e1485f05e24643
Laisheng Hu <Laisheng.Hu@csr.com>
2014-03-28 16:28:17 +0800

    video_codec:sirf:fix coding style warning
    Change-Id: Idfacdcde081c0b9605019083b6469733b968f3e6
    Signed-off-by: Laisheng Hu <Laisheng.Hu@csr.com>

--- arch/arm/mach-prima2/vxd39x.c
+++ arch/arm/mach-prima2/vxd39x.c
@@ -14,8 +14,10 @@ static int sirfsoc_video_codec_phy_size;
 void __init sirfsoc_video_codec_reserve_memblock(void)
 {
 	sirfsoc_video_codec_phy_size = 24 * SZ_1M;
-	sirfsoc_video_codec_phy_base = memblock_alloc(sirfsoc_video_codec_phy_size, PAGE_SIZE);
-	memblock_remove(sirfsoc_video_codec_phy_base, sirfsoc_video_codec_phy_size);
+	sirfsoc_video_codec_phy_base =
+		 memblock_alloc(sirfsoc_video_codec_phy_size, PAGE_SIZE);
+	memblock_remove(sirfsoc_video_codec_phy_base,
+			sirfsoc_video_codec_phy_size);
 }
 
 void sirfsoc_video_codec_get_mem(phys_addr_t *addr, int *size)
