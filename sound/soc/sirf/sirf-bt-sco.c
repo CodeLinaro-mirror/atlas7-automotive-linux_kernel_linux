652bda1b95f7779babcfa7513ea543d247726977
Bin Shi <Bin.Shi@csr.com>
2014-04-07 09:57:14 +0800

    drivers : sirf: fix build warning for minigps and bt-sco
    Fix return value check and useless value definition
    
    Change-Id: I9872c7bbd3a931e6294979de8a5ace190ed3247b
    Signed-off-by: Bin Shi <Bin.Shi@csr.com>

--- sound/soc/sirf/sirf-bt-sco.c
+++ sound/soc/sirf/sirf-bt-sco.c
@@ -14,7 +14,6 @@ static int sirf_bt_sco_hw_params(struct snd_pcm_substream *substream,
 	struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = rtd->codec_dai;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
 	struct snd_soc_codec *codec = rtd->codec;
 	struct snd_soc_card *card = codec->card;
