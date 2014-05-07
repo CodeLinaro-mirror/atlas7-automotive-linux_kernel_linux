4e7074114a367800485c4fb21ea88181abfa180d
Zhiwu Song <Zhiwu.Song@csr.com>
2014-05-07 10:27:42 +0800

    ARM: sirf: bring up smp in atlas7
    correct cpu1 jump/flag address
    remove scu related logic, scu don't export in cortex-a7, which export in cortex-a9
    move to irq_force_affinity support the affinity setting of per cpu timers in the early startup
    
    Change-Id: I95f1826e474e4e42110ffc137798935184c8056e
    Signed-off-by: Zhiwu Song <Zhiwu.Song@csr.com>

--- arch/arm/mach-prima2/common.c
+++ arch/arm/mach-prima2/common.c
@@ -255,7 +255,6 @@ static void __init sirfsoc_init_late(void)
 static __init void sirfsoc_map_io(void)
 {
 	sirfsoc_map_lluart();
-	sirfsoc_map_scu();
 }
 
 static void __init sirfsoc_init_irq(void)
