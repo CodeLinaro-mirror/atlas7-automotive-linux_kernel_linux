09ce43ec3a2cd3cef57db4801c089c857874d8f7
Guo Zeng <guo.zeng@csr.com>
2014-06-27 16:55:30 +0800

    RM: sirf: add atlas7 UART0 as a separate debug port
    This adds UART lowlevel debug options to atlas7,notice
    that non-secure OS use bit8 for rx empty fifo  status.
    And also UART0 is a separate debug port selected by OS.
    
    Change-Id: Ie768c466f646516ce9f659cf67806d248d0383e1
    Signed-off-by: Guo Zeng <guo.zeng@csr.com>

--- arch/arm/mach-prima2/lluart.c
+++ arch/arm/mach-prima2/lluart.c
@@ -12,26 +12,27 @@
 #include "common.h"
 
 #if defined(CONFIG_DEBUG_SIRFPRIMA2_UART1)
-#define SIRFSOC_UART1_PA_BASE          0xb0060000
+#define SIRF_LLUART_PA_BASE	0xb0060000
 #elif defined(CONFIG_DEBUG_SIRFMARCO_UART1)
-#define SIRFSOC_UART1_PA_BASE          0xcc060000
+#define SIRF_LLUART_PA_BASE	0xcc060000
 #elif defined(CONFIG_DEBUG_SIRFATLAS7_UART1)
-#define SIRFSOC_UART1_PA_BASE          0x18020000
+#define SIRF_LLUART_PA_BASE	0x18020000
+#elif defined(CONFIG_DEBUG_SIRFATLAS7_UART0)
+#define SIRF_LLUART_PA_BASE	0x18010000
 #else
-#define SIRFSOC_UART1_PA_BASE          0
+#define SIRF_LLUART_PA_BASE	0
 #endif
 
-#define SIRFSOC_UART1_VA_BASE          SIRFSOC_VA(SIRFSOC_UART1_PA_BASE & 0x000FFFFF)
-#define SIRFSOC_UART1_SIZE             SZ_4K
+#define SIRF_LLUART_VA_BASE	SIRFSOC_VA(SIRF_LLUART_PA_BASE & 0x000FFFFF)
+#define SIRF_LLUART_SIZE		SZ_4K
 
 void __init sirfsoc_map_lluart(void)
 {
 	struct map_desc sirfsoc_lluart_map = {
-		.virtual        = SIRFSOC_UART1_VA_BASE,
-		.pfn            = __phys_to_pfn(SIRFSOC_UART1_PA_BASE),
-		.length         = SIRFSOC_UART1_SIZE,
+		.virtual        = SIRF_LLUART_VA_BASE,
+		.pfn            = __phys_to_pfn(SIRF_LLUART_PA_BASE),
+		.length         = SIRF_LLUART_SIZE,
 		.type           = MT_DEVICE,
 	};
-
 	iotable_init(&sirfsoc_lluart_map, 1);
 }
