1a6779285571a4710b525c96bd934d53b4d7eac8
Zhiwu Song <Zhiwu.Song@csr.com>
2014-07-04 16:21:39 +0800

    mmc: sirf: cleanup dummy compatible atlas7 condtion check
    replace long compatible check with one short variable
    
    Change-Id: I3c4a81439ca6a2038dbf4b6548b9c362f893f69b
    Signed-off-by: Zhiwu Song <Zhiwu.Song@csr.com>
    [Merge gerrit 1696 and 1683]
    Signed-off-by: Barry Song <Baohua.Song@csr.com>

--- drivers/mmc/host/sdhci-sirf.c
+++ drivers/mmc/host/sdhci-sirf.c
@@ -84,7 +84,25 @@ static int sdhci_sirf_probe(struct platform_device *pdev)
 	int ret;
 
 	np = pdev->dev.of_node;
-	if (of_device_is_compatible(np, "sirf,atlas7-sdhc")) {
+
+	host = sdhci_pltfm_init(pdev, &sdhci_sirf_pdata,
+		sizeof(struct sdhci_sirf_priv));
+	if (IS_ERR(host)) {
+		ret = PTR_ERR(host);
+		goto err_sdhci_pltfm_init;
+	}
+
+	pltfm_host = sdhci_priv(host);
+	priv = sdhci_pltfm_priv(pltfm_host);
+	pltfm_host->priv = priv;
+	/* CSR refine for trig */
+	priv->loopdma = of_property_read_bool(pdev->dev.of_node, "loop-dma");
+
+	if (of_device_is_compatible(np, "sirf,atlas7-sdhc"))
+		priv->has_pclk = true;
+	else
+		priv->has_pclk = false;
+	if (priv->has_pclk) {
 		clk = devm_clk_get(&pdev->dev, "core");
 		if (IS_ERR(clk)) {
 			dev_err(&pdev->dev, "unable to get core clock");
@@ -108,20 +126,9 @@ static int sdhci_sirf_probe(struct platform_device *pdev)
 	else
 		gpio_cd = -EINVAL;
 
-	host = sdhci_pltfm_init(pdev, &sdhci_sirf_pdata, sizeof(struct sdhci_sirf_priv));
-	if (IS_ERR(host)) {
-		ret = PTR_ERR(host);
-		goto err_sdhci_pltfm_init;
-	}
-
-	pltfm_host = sdhci_priv(host);
-	priv = sdhci_pltfm_priv(pltfm_host);
-	pltfm_host->priv = priv;
-	/* CSR refine for trig */
-	priv->loopdma = of_property_read_bool(pdev->dev.of_node, "loop-dma");
 
 	priv->clk = clk;
-	if (of_device_is_compatible(np, "sirf,atlas7-sdhc"))
+	if (priv->has_pclk)
 		priv->pclk = pclk;
 	priv->gpio_cd = gpio_cd;
 
@@ -132,7 +139,7 @@ static int sdhci_sirf_probe(struct platform_device *pdev)
 	if (ret)
 		goto err_clk_prepare;
 
-	if (of_device_is_compatible(np, "sirf,atlas7-sdhc")) {
+	if (priv->has_pclk) {
 		ret = clk_prepare_enable(priv->pclk);
 		if (ret)
 			goto err_pclk_prepare;
@@ -179,7 +186,7 @@ static int sdhci_sirf_probe(struct platform_device *pdev)
 err_request_cd:
 	sdhci_remove_host(host, 0);
 err_sdhci_add:
-	if (of_device_is_compatible(np, "sirf,atlas7-sdhc"))
+	if (priv->has_pclk)
 		clk_disable_unprepare(priv->pclk);
 err_pclk_prepare:
 	clk_disable_unprepare(priv->clk);
@@ -201,7 +208,7 @@ static int sdhci_sirf_remove(struct platform_device *pdev)
 		mmc_gpio_free_cd(host->mmc);
 
 	clk_disable_unprepare(priv->clk);
-	if (of_device_is_compatible(pdev->dev.of_node, "sirf,atlas7-sdhc"))
+	if (priv->has_pclk)
 		clk_disable_unprepare(priv->pclk);
 
 	return 0;
@@ -220,7 +227,7 @@ static int sdhci_sirf_suspend(struct device *dev)
 		return ret;
 
 	clk_disable(priv->clk);
-	if (of_device_is_compatible(dev->of_node, "sirf,atlas7-sdhc"))
+	if (priv->has_pclk)
 		clk_disable(priv->pclk);
 
 	return 0;
@@ -239,7 +246,7 @@ static int sdhci_sirf_resume(struct device *dev)
 		return ret;
 	}
 
-	if (of_device_is_compatible(dev->of_node, "sirf,atlas7-sdhc")) {
+	if (priv->has_pclk) {
 		ret = clk_enable(priv->pclk);
 		if (ret) {
 			dev_dbg(dev, "Resume: Error enable interface clock\n");
