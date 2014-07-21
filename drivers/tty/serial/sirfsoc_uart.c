c6a0e5d430a9c7031fc4563fe92867366d1175de
Qipan Li <Qipan.Li@csr.com>
2014-07-21 18:13:46 +0800

    serial: sirf: en/disable TX_EN in start/stop_tx only for USP-UART
    commit f47c9ddb6fa8 ("serial: sirf: stop/start data transfer completely
    in PIO mode of stop_tx/start_tx") do TX_EN enable/disable work
    in start/stop_tx for USP-UART.
    
    UART with hardware flow control function will enable/disable TX_EN
    according to CTS's signal during transfer. so keep it just for USP-UART.
    
    Conflicts:
    	drivers/tty/serial/sirfsoc_uart.c
    
    Change-Id: Ic78a3e49f9070783712745c4b63d60390b79380e
    Signed-off-by: Qipan Li <Qipan.Li@csr.com>

--- drivers/tty/serial/sirfsoc_uart.c
+++ drivers/tty/serial/sirfsoc_uart.c
@@ -186,8 +186,9 @@ static void sirfsoc_uart_stop_tx(struct uart_port *port)
 				uint_en->sirfsoc_txfifo_empty_en);
 		}
 	} else {
-		wr_regl(port, ureg->sirfsoc_tx_rx_en, rd_regl(port,
-			ureg->sirfsoc_tx_rx_en) & ~SIRFUART_TX_EN);
+		if (sirfport->uart_reg->uart_type == SIRF_USP_UART)
+			wr_regl(port, ureg->sirfsoc_tx_rx_en, rd_regl(port,
+				ureg->sirfsoc_tx_rx_en) & ~SIRFUART_TX_EN);
 		if (!sirfport->is_marco)
 			wr_regl(port, ureg->sirfsoc_int_en_reg,
 				rd_regl(port, ureg->sirfsoc_int_en_reg) &
@@ -292,8 +293,9 @@ static void sirfsoc_uart_start_tx(struct uart_port *port)
 	if (sirfport->tx_dma_chan)
 		sirfsoc_uart_tx_with_dma(sirfport);
 	else {
-		wr_regl(port, ureg->sirfsoc_tx_rx_en, rd_regl(port,
-			ureg->sirfsoc_tx_rx_en) | SIRFUART_TX_EN);
+		if (sirfport->uart_reg->uart_type == SIRF_USP_UART)
+			wr_regl(port, ureg->sirfsoc_tx_rx_en, rd_regl(port,
+				ureg->sirfsoc_tx_rx_en) | SIRFUART_TX_EN);
 		sirfsoc_uart_pio_tx_chars(sirfport,
 			SIRFSOC_UART_IO_TX_REASONABLE_CNT);
 		wr_regl(port, ureg->sirfsoc_tx_fifo_op, SIRFUART_FIFO_START);
