6ec4acb0002892b31dfcf6472bc333fa7f264877
Minda Chen <Minda.Chen@csr.com>
2014-08-25 09:45:33 +0800

    mmc: sirf: correct the coding style
    remove the braces
    
    Change-Id: Iddc343951880d51e54be76d0fed7d46febcffc75
    Signed-off-by: Minda Chen <Minda.Chen@csr.com>

--- drivers/mmc/host/sdhci-sirf.c
+++ drivers/mmc/host/sdhci-sirf.c
@@ -39,14 +39,14 @@ static void sdhci_sirf_set_bus_width(struct sdhci_host *host, int width)
 	ctrl = sdhci_readb(host, SDHCI_HOST_CONTROL);
 	ctrl &= ~(SDHCI_CTRL_4BITBUS | SDHCI_SIRF_8BITBUS);
 
-	if (width == MMC_BUS_WIDTH_8) {
-		/*
-		 * CSR atlas7 and prima2 SD host version is not 3.0
-		 * 8bit-width enable bit of CSR SD hosts is 3,
-		 * while stardard hosts use bit 5
-		 */
+	/*
+	 * CSR atlas7 and prima2 SD host version is not 3.0
+	 * 8bit-width enable bit of CSR SD hosts is 3,
+	 * while stardard hosts use bit 5
+	 */
+	if (width == MMC_BUS_WIDTH_8)
 		ctrl |= SDHCI_SIRF_8BITBUS;
-	} else if (width == MMC_BUS_WIDTH_4)
+	else if (width == MMC_BUS_WIDTH_4)
 		ctrl |= SDHCI_CTRL_4BITBUS;
 
 	sdhci_writeb(host, ctrl, SDHCI_HOST_CONTROL);
