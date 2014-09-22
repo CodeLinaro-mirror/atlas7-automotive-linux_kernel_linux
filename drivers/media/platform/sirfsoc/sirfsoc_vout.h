377a78a2365cad65ca543cc9e4caf9f5695c4efd
Renwei Wu <renwei.wu@csr.com>
2014-09-22 15:28:25 +0800

    vout: sirf initial commit for sirfsoc video output devcie driver
    This patch adds sirfsoc video output device(vout) driver, vout driver creates a virtual device
    to v4l2 core subsystem as the video output device, the virtual device driver uses the sirf vdss
    core driver's callback to implent its function. The sirfsoc video output can be used to display
    the input of the capture driver, video image and the other still images.
    
    Change-Id: I12ce5d840cc746b936aae827f76771d613783092
    Signed-off-by: Renwei Wu <renwei.wu@csr.com>

---
+++ drivers/media/platform/sirfsoc/sirfsoc_vout.h
@@ -0,0 +1,60 @@
+/*
+ * CSR SiRFprima2 VIP host driver
+ * Copyright (c) 2011 - 2014 Cambridge Silicon Radio Limited, a CSR plc group
+ * company.
+ *
+ * Licensed under GPLv2 or later.
+ */
+
+#ifndef _SIRFSOC_VOUT_H_
+
+#define _SIRFSOC_VOUT_H_
+
+#include <video/sirfsoc_vdss.h>
+
+
+#define SIRFSOC_MAX_VOUT 2
+#define SIRFSOC_MAX_DISPLAY 2
+
+
+struct sirfsoc_vout_buf {
+	struct vb2_buffer vb;
+	struct list_head list;
+};
+
+struct sirfsoc_video_device {
+	struct platform_device *pdev;
+	struct v4l2_device v4l2_dev;
+	struct sirfsoc_vout_device *vouts[SIRFSOC_MAX_VOUT];
+
+	int num_panel;
+	struct sirfsoc_vdss_panel *display[SIRFSOC_MAX_DISPLAY];
+};
+
+struct sirfsoc_vout_device {
+	struct video_device *vd;
+	struct sirfsoc_video_device *vid_dev;
+	int opened;
+	spinlock_t vbq_lock;
+	struct mutex lock;
+	struct sirfsoc_vout_buf *disp_buf;
+	unsigned int numbuffers;
+
+	struct v4l2_pix_format pix_fmt;
+
+	enum v4l2_buf_type type;
+	struct vb2_queue vb2_q;
+	struct vb2_buffer *active_vb2_buf;
+	/* allocator-specific contexts for each plane */
+	struct vb2_alloc_ctx *alloc_ctx;
+	struct list_head dma_queue;
+	bool is_streaming;
+	struct v4l2_rect rect;
+	u32 surf_width;
+	u32 surf_height;
+	struct sirfsoc_vdss_panel *display;
+	struct sirfsoc_vdss_layer *layer;
+};
+
+
+#endif
