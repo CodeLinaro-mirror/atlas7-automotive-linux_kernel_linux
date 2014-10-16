e417eb7fd1e18999d72b12d81cea3aa9c081203a
Jiansong Chen <jiansong.chen@csr.com>
2014-10-16 17:36:53 +0800

    fb: sirf: add VBL support for pan_display to avoid display flicker.
    Change-Id: I17acb042b09c15ff797c266e4463a4bd676f38b6
    Signed-off-by: Jiansong Chen <jiansong.chen@csr.com>

--- drivers/video/fbdev/sirfsoc/fb/sirfsocfb.c
+++ drivers/video/fbdev/sirfsoc/fb/sirfsocfb.c
@@ -578,6 +578,8 @@ static int sirfsocfb_pan_display(struct fb_var_screeninfo *var,
 {
 	struct sirfsocfb_info *sfbi = FB2SFB(fbi);
 	struct fb_var_screeninfo new_var;
+	struct sirfsoc_vdss_panel *panel = fb2display(fbi);
+	struct sirfsoc_vdss_screen *scn;
 	int r;
 
 	DBG("pan_display(%d)\n", FB2SFB(fbi)->id);
@@ -597,6 +599,10 @@ static int sirfsocfb_pan_display(struct fb_var_screeninfo *var,
 	r = sirfsocfb_apply_changes(fbi, 0);
 
 	sirfsocfb_put_mem_region(sfbi->region);
+
+	scn = sirfsoc_vdss_find_screen_from_panel(panel);
+	if (var->activate & FB_ACTIVATE_VBL)
+		scn->wait_for_vsync(scn);
 
 	return r;
 }
