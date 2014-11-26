309a218f0e12b224aaf970a4224ce05a00fb7a7c
Minda Chen <Minda.Chen@csr.com>
2014-11-26 09:46:05 +0800

    sdhci: sirf: Enable NO_PRESCAN_POWERUP capability
    The capability description is "Don't power up before scan"
    In the scan stage driver will powe up again. So power up is
    redudant in driver init stage
    
    Change-Id: I5e11d9b206c75080f77040e42b9d70017bc119b1
    Signed-off-by: Minda Chen <Minda.Chen@csr.com>

--- drivers/mmc/host/sdhci-sirf.c
+++ drivers/mmc/host/sdhci-sirf.c
@@ -390,6 +390,7 @@ static int sdhci_sirf_probe(struct platform_device *pdev)
 	}
 
 	host->quirks2 = SDHCI_QUIRK2_SG_LIST_COMBINED_DMA_BUFFER;
+	host->mmc->caps2 |= MMC_CAP2_NO_PRESCAN_POWERUP;
 
 	ret = sdhci_add_host(host);
 	if (ret)
