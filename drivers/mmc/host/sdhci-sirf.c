10c8ddbed6b5a093f61e41890823390d816d6859
Minda Chen <Minda.Chen@csr.com>
2014-12-10 16:41:09 +0800

    Revert "sdhci: sirf: define get_power_config for SiRFSoc"
    commit b5c242342576 ("define get_power_config for SiRFSoc")
    is the preivous voltage change workaround method.
    Now it is not use. Revert it
    
    Change-Id: Ibc4777c4782a42062c7c39f7481d317b61bdce53
    Signed-off-by: Minda Chen <Minda.Chen@csr.com>

--- drivers/mmc/host/sdhci-sirf.c
+++ drivers/mmc/host/sdhci-sirf.c
@@ -125,12 +125,6 @@ static unsigned int sdhci_sirf_get_max_clk(struct sdhci_host *host)
 		return clk_get_rate(priv->clk);
 }
 
-static unsigned int sdhci_sirf_get_power_config(struct sdhci_host *host,
-	unsigned short power)
-{
-	return SDHCI_POWER_300;
-}
-
 static void sdhci_sirf_set_bus_width(struct sdhci_host *host, int width)
 {
 	u8 ctrl;
@@ -261,7 +255,6 @@ static struct sdhci_ops sdhci_sirf_ops = {
 	.platform_execute_tuning = sdhci_sirf_execute_tuning,
 	.set_clock = sdhci_set_clock,
 	.get_max_clock	= sdhci_sirf_get_max_clk,
-	.get_power_config  = sdhci_sirf_get_power_config,
 	.set_bus_width = sdhci_sirf_set_bus_width,
 	.reset = sdhci_reset,
 	.set_uhs_signaling = sdhci_set_uhs_signaling,
