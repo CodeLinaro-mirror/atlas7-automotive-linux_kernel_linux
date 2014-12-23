34d22402632a60660dc9deee10973399becd6f56
Barry Song <Baohua.Song@csr.com>
2014-12-23 16:10:02 +0800

    Revert "ARM: atlas7: csrvisor_tester: drop cache clean operations"
    This reverts commit bb2df95ea49a7e0e28c0b753d0002e2b4b7497ec.
    
    in newest csrvisor, uboot codes, we use uboot internal codes to switch
    from secure to non-secure mode, this switch happens after cache is OFF.
    Due to SRAM is almost as fast as L1 cache, and syscall is not frequent,
    this flush overhead should be very small.
    
    Signed-off-by: Barry Song <Baohua.Song@csr.com>

--- arch/arm/mach-prima2/csrvisor_test.c
+++ arch/arm/mach-prima2/csrvisor_test.c
@@ -31,6 +31,12 @@ static int csrvisor_test_thread(void *data)
 		msleep(2000);
 
 		cnt++;
+		/*
+		 * Secure API needs physical address
+		 * pointer for the parameters
+		 */
+		flush_cache_all();
+		outer_clean_range(__pa(&cnt), __pa(&cnt + 1));
 	}
 
 	return 0;
