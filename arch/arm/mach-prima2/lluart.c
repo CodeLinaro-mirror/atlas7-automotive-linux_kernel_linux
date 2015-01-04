b494279ebe0a8259ca9aa8afb524ca2262442072
Barry Song <Baohua.Song@csr.com>
2015-01-04 16:48:34 +0800

    ARM: sirf: refine DEBUG_LL to use DEBUG_UART_PHYS and DEBUG_UART_VIRT
    use DEBUG_UART_PHYS and DEBUG_UART_VIRT for sirfsoc low-level debug
    ports.
    
    Signed-off-by: Barry Song <Baohua.Song@csr.com>

--- arch/arm/mach-prima2/lluart.c
+++ arch/arm/mach-prima2/lluart.c
@@ -9,26 +9,14 @@
 #include <linux/kernel.h>
 #include <asm/page.h>
 #include <asm/mach/map.h>
-#include "common.h"
 
-#if defined(CONFIG_DEBUG_SIRFPRIMA2_UART1)
-#define SIRF_LLUART_PA_BASE	0xb0060000
-#elif defined(CONFIG_DEBUG_SIRFATLAS7_UART1)
-#define SIRF_LLUART_PA_BASE	0x18020000
-#elif defined(CONFIG_DEBUG_SIRFATLAS7_UART0)
-#define SIRF_LLUART_PA_BASE	0x18010000
-#else
-#define SIRF_LLUART_PA_BASE	0
-#endif
-
-#define SIRF_LLUART_VA_BASE	SIRFSOC_VA(SIRF_LLUART_PA_BASE & 0x000FFFFF)
-#define SIRF_LLUART_SIZE		SZ_4K
+#define SIRF_LLUART_SIZE               SZ_4K
 
 void __init sirfsoc_map_lluart(void)
 {
 	struct map_desc sirfsoc_lluart_map = {
-		.virtual        = SIRF_LLUART_VA_BASE,
-		.pfn            = __phys_to_pfn(SIRF_LLUART_PA_BASE),
+		.virtual        = CONFIG_DEBUG_UART_VIRT,
+		.pfn            = __phys_to_pfn(CONFIG_DEBUG_UART_PHYS),
 		.length         = SIRF_LLUART_SIZE,
 		.type           = MT_DEVICE,
 	};
