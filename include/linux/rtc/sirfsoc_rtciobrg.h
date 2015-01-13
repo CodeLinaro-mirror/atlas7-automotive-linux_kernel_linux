98f5a64fc2f2eecc8e5b27778899f0a24920d242
Barry Song <Baohua.Song@csr.com>
2015-01-13 17:09:19 +0800

    ARM: sirf: rtciobrg: fix build errors on regmap_config
    Fix the below:
    include/linux/rtc/sirfsoc_rtciobrg.h:19:22: warning: struct regmap_config declared inside parameter list
             const struct regmap_config *config);
                          ^
    include/linux/rtc/sirfsoc_rtciobrg.h:19:22: warning: its scope is only this definition or declaration, which is probably not what you want
      CC      drivers/rtc/rtc-sirfsoc.o
    In file included from drivers/input/misc/sirfsoc-onkey.c:15:0:
    include/linux/rtc/sirfsoc_rtciobrg.h:19:22: warning: struct regmap_config declared inside parameter list
             const struct regmap_config *config);
    
    In file included from drivers/power/sirfsoc_battery.c:30:0:
    include/linux/rtc/sirfsoc_rtciobrg.h:19:22: warning: struct regmap_config declared inside parameter list
             const struct regmap_config *config);
                          ^
    include/linux/rtc/sirfsoc_rtciobrg.h:19:22: warning: its scope is only this definition or declaration, which is probably not what you want
      LD      drivers/mfd/built-in.o
      LD      drivers/iio/adc/built-in.o
      LD      drivers/iio/built-in.o
    In file included from drivers/misc/a7ca_bt.c:6:0:
    include/linux/rtc/sirfsoc_rtciobrg.h:19:22: warning: struct regmap_config declared inside parameter list
             const struct regmap_config *config);
    
    Change-Id: I2010ff2d9c7dbf0c737874796352fa6a8ce740d1
    Signed-off-by: Barry Song <Baohua.Song@csr.com>

--- include/linux/rtc/sirfsoc_rtciobrg.h
+++ include/linux/rtc/sirfsoc_rtciobrg.h
@@ -9,6 +9,8 @@
 #ifndef _SIRFSOC_RTC_IOBRG_H_
 #define _SIRFSOC_RTC_IOBRG_H_
 
+struct regmap_config;
+
 extern void sirfsoc_rtc_iobrg_besyncing(void);
 
 extern u32 sirfsoc_rtc_iobrg_readl(u32 addr);
