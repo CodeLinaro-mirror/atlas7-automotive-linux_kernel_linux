cca7598d3c1497c3190eecef53f1972ac83ea2d8
Barry Song <Baohua.Song@csr.com>
2015-02-04 21:03:11 +0800

    ARM: sirf: fix nonsynchronous issues between local codes and mainline
    in many times of merging automatically or manually, we might import
    some nonsynchronous issues between local codes and mainline. git diff
    shows the files in this patch should be updated.
    
    Change-Id: I6285eaf222446d6c4136a64de8f5ecafebbdfe43
    Signed-off-by: Barry Song <Baohua.Song@csr.com>

--- arch/arm/include/asm/memory.h
+++ arch/arm/include/asm/memory.h
@@ -288,9 +288,7 @@ static inline void *phys_to_virt(phys_addr_t x)
  * Drivers should NOT use these either.
  */
 #define __pa(x)			__virt_to_phys((unsigned long)(x))
-#define __va(x)			((void *)__phys_to_virt((unsigned long)(x)))
-#define __pa_symbol(x)		__pa(RELOC_HIDE((unsigned long)(x),0))
-
+#define __va(x)			((void *)__phys_to_virt((phys_addr_t)(x)))
 #define pfn_to_kaddr(pfn)	__va((pfn) << PAGE_SHIFT)
 
 extern phys_addr_t (*arch_virt_to_idmap)(unsigned long x);
--- arch/arm/include/asm/system_misc.h
+++ arch/arm/include/asm/system_misc.h
@@ -10,7 +10,6 @@
 
 extern void cpu_init(void);
 
-void soft_restart_noirq(unsigned long);
 void soft_restart(unsigned long);
 extern void (*arm_pm_restart)(enum reboot_mode reboot_mode, const char *cmd);
 extern void (*arm_pm_idle)(void);
