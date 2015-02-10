83af1d32b8c29f875b844be5e48d643d55744561
Lihui Wen <Lihui.Wen@csr.com>
2015-02-10 19:04:48 +0800

    staging: android: ion: sirf: add sirf ion support
    Change-Id: Idc10f81e02549ac1226265368489d62f6410ee22
    Signed-off-by: Lihui Wen <Lihui.Wen@csr.com>

---
+++ drivers/staging/android/ion/sirfsoc/sirfsoc_ion.c
@@ -0,0 +1,100 @@
+/*
+ * CSR sirfsoc ion driver
+ *
+ * Copyright (c) 2011 - 2014 Cambridge Silicon Radio Limited, a CSR plc group
+ * company.
+ *
+ * Licensed under GPLv2 or later.
+ */
+
+#include <linux/module.h>
+#include <linux/err.h>
+#include <linux/platform_device.h>
+#include <linux/slab.h>
+#include "../ion.h"
+#include "../ion_priv.h"
+
+static struct ion_device *sirf_ion_device;
+static int num_heaps;
+static struct ion_heap **heaps;
+
+/* Wrap ion_client_create since client doesn't know sirf_ion_device */
+struct ion_client *sirfsoc_ion_client_create(const char *name)
+{
+	if (IS_ERR_OR_NULL(sirf_ion_device))
+		return NULL;
+
+	return ion_client_create(sirf_ion_device, name);
+}
+EXPORT_SYMBOL(sirfsoc_ion_client_create);
+
+static int sirfsoc_ion_probe(struct platform_device *pdev)
+{
+	struct ion_platform_data *pdata = pdev->dev.platform_data;
+	int err;
+	int i;
+
+	num_heaps = pdata->nr;
+
+	heaps = devm_kzalloc(&pdev->dev,
+			     sizeof(struct ion_heap *) * pdata->nr,
+			     GFP_KERNEL);
+
+	sirf_ion_device = ion_device_create(NULL);
+	if (IS_ERR_OR_NULL(sirf_ion_device))
+		return PTR_ERR(sirf_ion_device);
+
+	/* create the heaps as specified in the board file */
+	for (i = 0; i < num_heaps; i++) {
+		struct ion_platform_heap *heap_data = &pdata->heaps[i];
+
+		heaps[i] = ion_heap_create(heap_data);
+		if (IS_ERR_OR_NULL(heaps[i])) {
+			err = PTR_ERR(heaps[i]);
+			goto err;
+		}
+		ion_device_add_heap(sirf_ion_device, heaps[i]);
+	}
+	platform_set_drvdata(pdev, sirf_ion_device);
+	return 0;
+err:
+	for (i = 0; i < num_heaps; i++) {
+		if (heaps[i])
+			ion_heap_destroy(heaps[i]);
+	}
+	return err;
+}
+
+static int sirfsoc_ion_remove(struct platform_device *pdev)
+{
+	struct ion_device *sirf_ion_device = platform_get_drvdata(pdev);
+	int i;
+
+	ion_device_destroy(sirf_ion_device);
+	for (i = 0; i < num_heaps; i++)
+		ion_heap_destroy(heaps[i]);
+	return 0;
+}
+
+static struct platform_driver sirfsoc_ion_driver = {
+	.probe = sirfsoc_ion_probe,
+	.remove = sirfsoc_ion_remove,
+	.driver = { .name = "sirf-ion" }
+};
+
+static int __init sirfsoc_ion_init(void)
+{
+	return platform_driver_register(&sirfsoc_ion_driver);
+}
+
+static void __exit sirfsoc_ion_exit(void)
+{
+	platform_driver_unregister(&sirfsoc_ion_driver);
+}
+
+module_init(sirfsoc_ion_init);
+module_exit(sirfsoc_ion_exit);
+
+MODULE_AUTHOR("Lihui Wen <lihui.wen@csr.com>");
+MODULE_DESCRIPTION("SIRF ION Driver");
+MODULE_LICENSE("GPL");
