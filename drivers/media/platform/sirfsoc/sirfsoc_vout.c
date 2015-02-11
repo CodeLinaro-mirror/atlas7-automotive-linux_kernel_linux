e3ff19ffdd62aec55c6db56bcaf0fd93a2a0372f
Guchun Chen <guchun.chen@csr.com>
2015-02-11 10:52:39 +0800

    vout: sirf: correct bytesperline setting in v4l2_pix_format.
    Change-Id: I91583946e96e8eb6101871ecd79af426e2172821
    Signed-off-by: Guchun Chen <guchun.chen@csr.com>
    Signed-off-by: xf02 <xiaofeng.fei@csr.com>

--- drivers/media/platform/sirfsoc/sirfsoc_vout.c
+++ drivers/media/platform/sirfsoc/sirfsoc_vout.c
@@ -265,7 +265,7 @@ static int __sirfsoc_vout_try_fmt(struct v4l2_pix_format *pix, u32 *hor_stride,
 	u32 *ver_stride)
 {
 	int index = 0;
-	int bpp = 0; /* bits per pixel */
+	int bpp = 0; /* byte per pixel */
 	int vdss_pixfmt;
 
 	pix->width = clamp(pix->width, (u32)VIDEO_MIN_WIDTH,
@@ -292,24 +292,29 @@ static int __sirfsoc_vout_try_fmt(struct v4l2_pix_format *pix, u32 *hor_stride,
 	case V4L2_PIX_FMT_VYUY:
 	case V4L2_PIX_FMT_UYVY:
 		pix->colorspace = V4L2_COLORSPACE_JPEG;
-		bpp = 16;
+		bpp = 2;
+		break;
 	case V4L2_PIX_FMT_NV12:
 	case V4L2_PIX_FMT_NV21:
 	case V4L2_PIX_FMT_YUV420:
 		pix->colorspace = V4L2_COLORSPACE_JPEG;
-		bpp = 12;
+		/*
+		 * Note: When the image format is planar, the bytesperline
+		 * value applies to the largest plane, so here set bpp to be 1
+		 */
+		bpp = 1;
 		break;
 	case V4L2_PIX_FMT_RGB565:
 		pix->colorspace = V4L2_COLORSPACE_SRGB;
-		bpp = 16;
+		bpp = 2;
 		break;
 	case V4L2_PIX_FMT_RGB32:
 		pix->colorspace = V4L2_COLORSPACE_SRGB;
-		bpp = 32;
+		bpp = 4;
 		break;
 	default:
 		pix->colorspace = V4L2_COLORSPACE_SRGB;
-		bpp = 16;
+		bpp = 2;
 		break;
 	}
 
@@ -318,9 +323,20 @@ static int __sirfsoc_vout_try_fmt(struct v4l2_pix_format *pix, u32 *hor_stride,
 	__sirfsoc_vout_alignment(vdss_pixfmt, pix->width, pix->height,
 		hor_stride, ver_stride);
 
-	pix->bytesperline = *hor_stride * bpp / 8;
+	pix->bytesperline = *hor_stride * bpp;
 
 	pix->sizeimage = pix->bytesperline * (*ver_stride);
+
+	switch (vdss_pixfmt) {
+	case VDSS_PIXELFORMAT_NV12:
+	case VDSS_PIXELFORMAT_NV21:
+	case VDSS_PIXELFORMAT_I420:
+		/* Planar format should contain Y and UV sections */
+		pix->sizeimage += pix->sizeimage >> 1;
+		break;
+	default:
+		break;
+	}
 
 	return 0;
 }
