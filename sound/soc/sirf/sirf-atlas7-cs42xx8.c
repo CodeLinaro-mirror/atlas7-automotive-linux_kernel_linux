4962989c87ddbe9e471ead718925c8f2dcf7e720
Yibo Cai <yibo.cai@csr.com>
2015-03-11 10:04:32 +0800

    ASoC: sirf: fix I2S one line mode reversed logic
    Change-Id: I2d00610ab2ca4dce9f3d0eb0e951675f93cceff8
    Signed-off-by: Yibo Cai <yibo.cai@csr.com>

--- sound/soc/sirf/sirf-atlas7-cs42xx8.c
+++ sound/soc/sirf/sirf-atlas7-cs42xx8.c
@@ -28,7 +28,7 @@ static int sirf_hdmi_init(struct snd_soc_pcm_runtime *rtd)
 	/* Enable slot#0 audio pins */
 	gpio_set_value_cansleep(data->gpio_sw1, 0);
 	gpio_set_value_cansleep(data->gpio_sw2, 1);
-	if (data->olm)
+	if (!data->olm)
 		gpio_set_value_cansleep(data->gpio_sw3, 1);
 
 	return 0;
@@ -140,13 +140,13 @@ static int sirf_hdmi_card_probe(struct platform_device *pdev)
 	data->gpio_rst = of_get_named_gpio(pdev->dev.of_node, "ext-rst", 0);
 	data->gpio_sw1 = of_get_named_gpio(pdev->dev.of_node, "sw1-sel", 0);
 	data->gpio_sw2 = of_get_named_gpio(pdev->dev.of_node, "sw2-sel", 0);
-	if (data->olm)
+	if (!data->olm)
 		data->gpio_sw3 = of_get_named_gpio(pdev->dev.of_node,
 				"sw3-sel", 0);
 	if (!gpio_is_valid(data->gpio_rst) ||
 			!gpio_is_valid(data->gpio_sw1) ||
 			!gpio_is_valid(data->gpio_sw2) ||
-			(data->olm && !gpio_is_valid(data->gpio_sw3))) {
+			(!data->olm && !gpio_is_valid(data->gpio_sw3))) {
 		dev_err(&pdev->dev, "Failed to parse GPIO pins\n");
 		return -EINVAL;
 	}
@@ -157,7 +157,7 @@ static int sirf_hdmi_card_probe(struct platform_device *pdev)
 			GPIOF_OUT_INIT_LOW, "sw1-sel");
 	ret |= devm_gpio_request_one(&pdev->dev, data->gpio_sw2,
 			GPIOF_OUT_INIT_HIGH, "sw2-sel");
-	if (data->olm)
+	if (!data->olm)
 		ret |= devm_gpio_request_one(&pdev->dev, data->gpio_sw3,
 				GPIOF_OUT_INIT_HIGH, "sw3-sel");
 	if (ret) {
