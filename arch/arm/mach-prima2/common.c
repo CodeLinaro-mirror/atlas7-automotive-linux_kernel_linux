4e454ea1b88d71ef1675157559edefabd176e7b9
Yibo Cai <yibo.cai@csr.com>
2015-04-15 13:02:46 +0800

    ARM: prima2: remove bt-sco platform device
    Dig out this secret when adding a2dp to current bt-sco driver.
    This legacy code is useless now on prima2, atlas6, atlas7.
    
    Change-Id: I3ebe796d0a2a318c5f73a1c808c081cff2614875
    Signed-off-by: Yibo Cai <yibo.cai@csr.com>

--- arch/arm/mach-prima2/common.c
+++ arch/arm/mach-prima2/common.c
@@ -109,7 +109,6 @@ static void __init sirfsoc_init_mach(void)
 		sirf_auxdata_lookup, NULL);
 
 	platform_device_register_simple("cpufreq-cpu0", -1, NULL, 0);
-	platform_device_register_simple("bt-sco", -1, NULL, 0);
 
 	sirfsoc_noc_init();
 }
