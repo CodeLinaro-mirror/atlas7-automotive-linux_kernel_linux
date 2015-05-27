e796ad863183a0f81cf1298d8e3c7df0eba398ee
Wei Chen <Wei.Chen@csr.com>
2015-05-27 13:44:45 +0800

    drivers:hwspinlock:remove useless header files of atlas7 hwlock
    Some header files are useless for atlas7 hwlock driver.
    remove them to avoid confusion.
    
    Change-Id: I1fdf034adff55f23bbe6319405b2dc2959c2b5e2
    Signed-off-by: Wei Chen <Wei.Chen@csr.com>

--- drivers/hwspinlock/sirf_hwspinlock.c
+++ drivers/hwspinlock/sirf_hwspinlock.c
@@ -6,15 +6,9 @@
  * Licensed under GPLv2.
  */
 
-#include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/device.h>
-#include <linux/delay.h>
 #include <linux/io.h>
-#include <linux/bitops.h>
 #include <linux/pm_runtime.h>
-#include <linux/slab.h>
-#include <linux/spinlock.h>
 #include <linux/hwspinlock.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
