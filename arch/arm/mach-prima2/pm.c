d6d360503d6f075c10bcb3365fbf7260f430334c
Guo Zeng <guo.zeng@csr.com>
2015-06-10 15:07:54 +0800

    pm: sirf: fix platform restart delay
    currently use  watchdog as workaround for platform
    restart, but the watchdog configure sequence might
    wrong. which cause restart delay more than 15 sec.
    the correct sequence should be:
    1.clear the counter
    2.set enough match  value
    3.enable the counter control
    4.enable the watchdog reset feature of the counter
    the  reason for  enough match is that, after 3, the counter
    start kicking, and before 4 is done, the counter is run at
    150M clk, so the match should be large enough.
    
    with the patch the restart delay is less than 1/8.
    
    Change-Id: I5850c35ebf01d1489aaf6ce5327157e9c3ad9a7c
    Signed-off-by: Guo Zeng <guo.zeng@csr.com>

--- arch/arm/mach-prima2/pm.c
+++ arch/arm/mach-prima2/pm.c
@@ -339,12 +339,11 @@ static const struct of_device_id sirfsoc_pm_ids[] = {
 void sirfsoc_atlas7_restart(enum reboot_mode mode, const char *cmd)
 {
 #define CPU_CLK_SEL 0xf8
-#define WDOG_CNT64_LATCH_LO 0x7c
 #define WDOG_MATCH 0x18
 #define WDOG_TIMER_WDT_INDEX		5
 #define WDOG_EN 0x64
 #define WDOG_CNT_CTRL 0x0
-
+#define WDOG_CNT	0x48
 	/*
 	* set retain register as 0x2 for reset, so that uboot can
 	* disdinguish between real watchdog event and this workaroad
@@ -355,19 +354,17 @@ void sirfsoc_atlas7_restart(enum reboot_mode mode, const char *cmd)
 	/* workaround reset for atlas7 */
 	writel(0, sinfo->clkc_base + CPU_CLK_SEL);
 
+	/* update timeout for match */
+	writel(0, sinfo->timer_base + WDOG_CNT +
+		4 * WDOG_TIMER_WDT_INDEX);
+	writel(0x10000000,	sinfo->timer_base + WDOG_MATCH +
+			4 * WDOG_TIMER_WDT_INDEX);
 	/* enable watchdog */
-	writel(readl(sinfo->timer_base + WDOG_CNT_CTRL +
-			4 * WDOG_TIMER_WDT_INDEX) | 0x3,
-		sinfo->timer_base + WDOG_CNT_CTRL +
+	writel(0x3, sinfo->timer_base + WDOG_CNT_CTRL +
 			4 * WDOG_TIMER_WDT_INDEX);
 	writel(1, sinfo->timer_base + WDOG_EN);
-
-	/* update timeout for match */
-	writel(readl(sinfo->timer_base + WDOG_CNT64_LATCH_LO) +
-			0x3,
-		sinfo->timer_base + WDOG_MATCH +
-			4 * WDOG_TIMER_WDT_INDEX);
-
+	while (1)
+		;
 }
 
 static int atlas7_pm_retain_init(struct sirfsoc_pm_init_t *pinit)
