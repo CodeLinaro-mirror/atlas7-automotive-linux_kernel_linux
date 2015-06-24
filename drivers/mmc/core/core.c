de065fc2775d58a86a78d841f4ec7c7a1a26003f
as64 <Andy.Sun@csr.com>
2015-06-24 17:09:13 +0800

    mmc: core: bind mmc detection to CPU1
    Bind mmc detect thread to CPU1 to avoid the interrupt-off context
    from blocking VIP interrupt too long.
    
    For atlas7, we have some interrupt latency requirement from
    VIP, the "broken-cd" of SD makes us miss the deadline of
    VIP interrupt, which will cause VIP FIFO overflow here, we
    make sure "broken-cd" interrupt-off context is in different CPU with VIP.
    
    Change-Id: I78bf82a732e1cb5f2b48749d65063a6b9837202a
    Signed-off-by: as64 <Andy.Sun@csr.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -71,6 +71,16 @@ module_param(use_spi_crc, bool, 0);
 static int mmc_schedule_delayed_work(struct delayed_work *work,
 				     unsigned long delay)
 {
+	/*
+	* for atlas7, we have some interrupt latency requirement from
+	* VIP, the "broken-cd" of SD makes us miss the deadline of
+	* VIP interrupt, which will cause VIP FIFO overflow
+	* here, we make sure "broken-cd" interrupt-off context is in
+	* different CPU with VIP
+	*/
+	if (cpu_online(1) && of_machine_is_compatible("sirf,atlas7"))
+		return queue_delayed_work_on(1, workqueue, work, delay);
+
 	return queue_delayed_work(workqueue, work, delay);
 }
 
@@ -2670,11 +2680,21 @@ void mmc_init_context_info(struct mmc_host *host)
 	init_waitqueue_head(&host->context_info.wait);
 }
 
+/*
+ * for atlas7, we want to bind mmc detect thread to CPU1 to avoid
+ * the interrupt-off context in this thread from blocking VIP
+ * interrupt too long
+ */
+#define alloc_mmc_workqueue(fmt, flags, args...) \
+	(of_machine_is_compatible("sirf,atlas7") ? \
+		alloc_workqueue(fmt, __WQ_ORDERED | (flags), 1, ##args) : \
+		alloc_ordered_workqueue(fmt, flags, ##args))
+
 static int __init mmc_init(void)
 {
 	int ret;
 
-	workqueue = alloc_ordered_workqueue("kmmcd", 0);
+	workqueue = alloc_mmc_workqueue("kmmcd", 0);
 	if (!workqueue)
 		return -ENOMEM;
 
