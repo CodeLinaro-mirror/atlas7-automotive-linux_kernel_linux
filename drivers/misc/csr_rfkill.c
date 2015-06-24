6a39631ef3c14338c0026262db503019e04da4d9
Peter Zhang <peter.zhang@csr.com>
2015-06-24 19:05:16 +0800

    csr_rfkill: use rfkill interface to control amber power
    move wlan and BT rfkill control to single file - csr_rfkill.c
    And modify dts files to match the gpio pin.
    
    Change-Id: Ifab16ec6829623cb59abaddf12cec2d97bb05b7a
    Signed-off-by: Peter Zhang <peter.zhang@csr.com>

--- drivers/misc/csr_rfkill.c
+++ drivers/misc/csr_rfkill.c
@@ -21,14 +21,21 @@
 #include <linux/slab.h>
 
 
-#define GPIO_POWER_PIN_NAME		"bt_gpio_power"
-#define GPIO_POWER_PIN_LABEL	"bt power gpio"
-#define GPIO_RESET_PIN_NAME		"bt_gpio_reset"
-#define GPIO_RESET_PIN_LABEL	"bt reset gpio"
+#define GPIO_POWER_9300_PIN_NAME		"amber9300_gpio_power"
+#define GPIO_POWER_9300_PIN_LABEL		"amber9300 power gpio"
+#define GPIO_RESET_9300_PIN_NAME		"amber9300_gpio_reset"
+#define GPIO_RESET_9300_PIN_LABEL		"amber9300 reset gpio"
+
+#define GPIO_POWER_PIN_NAME     "bt_gpio_power"
+#define GPIO_POWER_PIN_LABEL    "bt power gpio"
+#define GPIO_RESET_PIN_NAME     "bt_gpio_reset"
+#define GPIO_RESET_PIN_LABEL    "bt reset gpio"
+
 
 
 struct rfkill_gpio_data {
-	struct rfkill *rfkill_dev;
+	struct rfkill *rfkill_bt_dev;
+	struct rfkill *rfkill_wlan_dev;
 	struct pwm_device *pwm;
 	int power_gpio;
 	int reset_gpio;
@@ -39,12 +46,12 @@ struct rfkill_gpio_data {
 	void (*power_off)(struct rfkill_gpio_data *rfkill);
 };
 
-struct csr_bt_register {
+struct csr_connectivity_register {
 	int power_delay;
 	int reset_delay;
-	int (*bt_probe)(struct rfkill_gpio_data *rfkill,
+	int (*probe)(struct rfkill_gpio_data *rfkill,
 					struct platform_device *pdev);
-	void (*bt_remove)(struct rfkill_gpio_data *rfkill,
+	void (*remove)(struct rfkill_gpio_data *rfkill,
 					struct platform_device *pdev);
 	void (*power_on)(struct rfkill_gpio_data *rfkill);
 	void (*power_off)(struct rfkill_gpio_data *rfkill);
@@ -111,9 +118,10 @@ static int csr_9300_probe(struct rfkill_gpio_data *rfkill,
 	struct device_node *dn = pdev->dev.of_node;
 	int ret = 0;
 
-	rfkill->power_gpio = of_get_named_gpio(dn, GPIO_POWER_PIN_NAME, 0);
+	rfkill->power_gpio = of_get_named_gpio(dn, GPIO_POWER_9300_PIN_NAME, 0);
 	if (gpio_is_valid(rfkill->power_gpio)) {
-		ret = gpio_request(rfkill->power_gpio, GPIO_POWER_PIN_LABEL);
+		ret = gpio_request(rfkill->power_gpio,
+				GPIO_POWER_9300_PIN_LABEL);
 		if (ret) {
 			pr_warn("%s: failed to get power gpio.\n",
 				__func__);
@@ -121,9 +129,10 @@ static int csr_9300_probe(struct rfkill_gpio_data *rfkill,
 		}
 	}
 
-	rfkill->reset_gpio = of_get_named_gpio(dn, GPIO_RESET_PIN_NAME, 0);
+	rfkill->reset_gpio = of_get_named_gpio(dn, GPIO_RESET_9300_PIN_NAME, 0);
 	if (gpio_is_valid(rfkill->reset_gpio)) {
-		ret = gpio_request(rfkill->reset_gpio, GPIO_RESET_PIN_LABEL);
+		ret = gpio_request(rfkill->reset_gpio,
+				GPIO_RESET_9300_PIN_LABEL);
 		if (ret) {
 			pr_warn("%s: failed to get reset gpio.\n",
 				__func__);
@@ -200,27 +209,27 @@ static void csr_8311_remove(struct rfkill_gpio_data *rfkill,
 
 }
 
-static struct csr_bt_register csr_bt_9300 = {
+static struct csr_connectivity_register csr_amber_9300 = {
 	.power_delay = 10,
 	.reset_delay = 400,
-	.bt_probe = csr_9300_probe,
-	.bt_remove = csr_9300_remove,
+	.probe = csr_9300_probe,
+	.remove = csr_9300_remove,
 	.power_on = csr_9300_power_on,
 	.power_off = csr_9300_power_off,
 };
 
 
-static struct csr_bt_register csr_bt_8311 = {
+static struct csr_connectivity_register csr_bt_8311 = {
 	.power_delay = 0,
 	.reset_delay = 0,
-	.bt_probe = csr_8311_probe,
-	.bt_remove = csr_8311_remove,
+	.probe = csr_8311_probe,
+	.remove = csr_8311_remove,
 	.power_on = csr_8311_power_on,
 	.power_off = csr_8311_power_off,
 };
 
-static const struct of_device_id csr_bt_of_match[] = {
-	{ .compatible = "sirf,bt-9300", .data = &csr_bt_9300 },
+static const struct of_device_id csr_rfkill_of_match[] = {
+	{ .compatible = "sirf,amber-9300", .data = &csr_amber_9300 },
 	{ .compatible = "sirf,bt-8311", .data = &csr_bt_8311 },
 	{ .compatible = "sirf,bt-8311-evb" },
 	{},
@@ -266,21 +275,21 @@ static const struct rfkill_ops rfkill_gpio_ops = {
 	.set_block = rfkill_gpio_set_power,
 };
 
-static int csr_bt_probe(struct platform_device *pdev)
+static int csr_rfkill_probe(struct platform_device *pdev)
 {
 	struct rfkill_gpio_data *rfkill = NULL;
 	const struct of_device_id *match;
-	struct csr_bt_register *bt_reg;
+	struct csr_connectivity_register *reg_ops;
 	int ret = 0;
 
-	match = of_match_node(csr_bt_of_match, pdev->dev.of_node);
+	match = of_match_node(csr_rfkill_of_match, pdev->dev.of_node);
 	if (!match) {
 		dev_err(&pdev->dev, "no matching node\n");
 		ret = -ENODEV;
 		return ret;
 	}
 
-	bt_reg = (struct csr_bt_register *)match->data;
+	reg_ops = (struct csr_connectivity_register *)match->data;
 
 	rfkill = kzalloc(sizeof(*rfkill), GFP_KERNEL);
 	if (!rfkill) {
@@ -288,43 +297,61 @@ static int csr_bt_probe(struct platform_device *pdev)
 		goto fail_alloc;
 	}
 
-	if (!(bt_reg->bt_probe)) {
+	if (!(reg_ops->probe)) {
 		ret = -EINVAL;
 		goto fail_alloc;
 	}
 
-	ret = bt_reg->bt_probe(rfkill, pdev);
+	ret = reg_ops->probe(rfkill, pdev);
 
 	if (ret != 0)
 		goto fail_reset;
 
-	rfkill->rfkill_dev = rfkill_alloc("csr-bt-chip", &pdev->dev,
+	/* register RFKILL_TYPE_BLUETOOTH for rfkill_bt_dev */
+	rfkill->rfkill_bt_dev = rfkill_alloc("csr-bt-chip", &pdev->dev,
 					  RFKILL_TYPE_BLUETOOTH,
 					  &rfkill_gpio_ops, rfkill);
 
-	if (!rfkill->rfkill_dev) {
+	if (!rfkill->rfkill_bt_dev) {
 		ret = -ENOMEM;
 		goto fail_reset;
 	}
 
-	rfkill->power_on = bt_reg->power_on;
-	rfkill->power_off = bt_reg->power_off;
-
-	rfkill->power_delay = bt_reg->power_delay;
-	rfkill->reset_delay = bt_reg->reset_delay;
-
-
-	ret = rfkill_register(rfkill->rfkill_dev);
+	ret = rfkill_register(rfkill->rfkill_bt_dev);
 	if (ret < 0)
-		goto fail_rfkill;
+		goto fail_bt_rfkill;
+
+	/* register RFKILL_TYPE_WLAN for rfkill_wlan_dev */
+	rfkill->rfkill_wlan_dev = rfkill_alloc("csr-wlan-chip", &pdev->dev,
+					  RFKILL_TYPE_WLAN,
+					  &rfkill_gpio_ops, rfkill);
+
+	if (!rfkill->rfkill_wlan_dev) {
+		ret = -ENOMEM;
+		goto fail_reset;
+	}
+
+	ret = rfkill_register(rfkill->rfkill_wlan_dev);
+	if (ret < 0)
+		goto fail_wlan_rfkill;
+
+	rfkill->power_on = reg_ops->power_on;
+	rfkill->power_off = reg_ops->power_off;
+
+	rfkill->power_delay = reg_ops->power_delay;
+	rfkill->reset_delay = reg_ops->reset_delay;
 
 	platform_set_drvdata(pdev, rfkill);
 
 	return 0;
 
-fail_rfkill:
-	if (rfkill->rfkill_dev != NULL)
-		rfkill_destroy(rfkill->rfkill_dev);
+fail_wlan_rfkill:
+	if (rfkill->rfkill_wlan_dev != NULL)
+		rfkill_destroy(rfkill->rfkill_wlan_dev);
+
+fail_bt_rfkill:
+	if (rfkill->rfkill_bt_dev != NULL)
+		rfkill_destroy(rfkill->rfkill_bt_dev);
 fail_reset:
 	if (gpio_is_valid(rfkill->reset_gpio))
 		gpio_free(rfkill->reset_gpio);
@@ -336,47 +363,51 @@ fail_alloc:
 	return ret;
 }
 
-static int csr_bt_remove(struct platform_device *pdev)
+static int csr_rfkill_remove(struct platform_device *pdev)
 {
 	const struct of_device_id *match;
-	struct csr_bt_register *bt_reg;
+	struct csr_connectivity_register *reg_ops;
 	int ret = 0;
 	struct rfkill_gpio_data *rfkill = platform_get_drvdata(pdev);
 
 	if (!rfkill)
 		return ret;
 
-	match = of_match_node(csr_bt_of_match, pdev->dev.of_node);
+	match = of_match_node(csr_rfkill_of_match, pdev->dev.of_node);
 	if (!match) {
 		dev_err(&pdev->dev, "no matching node\n");
 		ret = -ENODEV;
 		return ret;
 	}
 
-	bt_reg = (struct csr_bt_register *)match->data;
+	reg_ops = (struct csr_connectivity_register *)match->data;
 
-	rfkill_unregister(rfkill->rfkill_dev);
-	rfkill_destroy(rfkill->rfkill_dev);
+	/* remove rfkill bt device */
+	rfkill_unregister(rfkill->rfkill_bt_dev);
+	rfkill_destroy(rfkill->rfkill_bt_dev);
+	/* remove rfkill wlan device */
+	rfkill_unregister(rfkill->rfkill_wlan_dev);
+	rfkill_destroy(rfkill->rfkill_wlan_dev);
 
-	if (bt_reg->bt_remove)
-		bt_reg->bt_remove(rfkill, pdev);
+	if (reg_ops->remove)
+		reg_ops->remove(rfkill, pdev);
 
 	kfree(rfkill);
 
 	return ret;
 }
 
-static struct platform_driver csr_bt_driver = {
+static struct platform_driver csr_rfkill_driver = {
 	.driver = {
-		.name = "csr-bt-rfkill",
+		.name = "csr-connectivity-rfkill",
 		.owner = THIS_MODULE,
-		.of_match_table = csr_bt_of_match,
+		.of_match_table = csr_rfkill_of_match,
 		},
-	.probe = csr_bt_probe,
-	.remove = csr_bt_remove,
+	.probe = csr_rfkill_probe,
+	.remove = csr_rfkill_remove,
 };
 
-module_platform_driver(csr_bt_driver);
+module_platform_driver(csr_rfkill_driver);
 
 MODULE_AUTHOR("Cambridge Silicon Radio Ltd");
 MODULE_DESCRIPTION("CSR Synergy for Bluetooth and WLAN Enable Driver");
