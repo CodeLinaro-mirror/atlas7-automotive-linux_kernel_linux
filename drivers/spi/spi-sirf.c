0e0d0b3945115bac1b590ca6aa4cfdb19424f08c
Qipan Li <Qipan.Li@csr.com>
2015-07-07 16:54:47 +0800

    spi: sirf: fix null address reference
    in old way sspi->fifo_size = spi_comp_data->fifo_size; spi_comp_data
    be assigned after the statement.
    
    Change-Id: Icf166a898aea1867ada87356e825d1d87f1becf2
    Signed-off-by: Qipan Li <Qipan.Li@csr.com>

--- drivers/spi/spi-sirf.c
+++ drivers/spi/spi-sirf.c
@@ -1086,9 +1086,9 @@ static int spi_sirfsoc_probe(struct platform_device *pdev)
 	match = of_match_node(spi_sirfsoc_of_match, pdev->dev.of_node);
 	platform_set_drvdata(pdev, master);
 	sspi = spi_master_get_devdata(master);
+	spi_comp_data = (struct sirf_spi_comp_data *)match->data;
 	sspi->fifo_size = spi_comp_data->fifo_size;
 	sspi->fifo_full_offset = ilog2(sspi->fifo_size);
-	spi_comp_data = (struct sirf_spi_comp_data *)match->data;
 	sspi->regs = spi_comp_data->regs;
 	sspi->type = spi_comp_data->type;
 	sspi->fifo_level_chk_mask = (sspi->fifo_size / 4) - 1;
