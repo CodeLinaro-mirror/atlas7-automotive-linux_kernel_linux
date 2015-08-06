cb7f22d67fe6b29d077eacd9fd446a7ae138baa5
Guo Zeng <guo.zeng@csr.com>
2015-08-06 17:15:58 +0800

    clk: atlas7: add clk for dspub and pre-enable clk for csrvisor
    ccpub_sec is used by dspub, so add in dts.
    ccsec_sec is used by  csrvisor. need to pre-enable it. now this clock is
    enabled in bootrom.
    
    Change-Id: Id3c369e791a9867ed2589b67a95d88ea9b236c4d
    Signed-off-by: Guo Zeng <guo.zeng@csr.com>

--- drivers/clk/sirf/clk-atlas7.c
+++ drivers/clk/sirf/clk-atlas7.c
@@ -1310,6 +1310,7 @@ static void __init atlas7_clk_init(struct device_node *np)
 				!strcmp(unit->unit_name, "cpum_cpu") ||
 				!strcmp(unit->unit_name, "sys2pci2_io") ||
 				!strcmp(unit->unit_name, "dmac3_kas") ||
+				!strcmp(unit->unit_name, "ccsec_sec") ||
 				!strcmp(unit->unit_name, "sys2pci_io"))
 			clk_prepare_enable(atlas7_clks[i]);
 	}
