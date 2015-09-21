ec9f6a2c9016ef3ca8750d3358e983542c31d7c0
Wei Chen <cawei@codeaurora.org>
2015-09-21 15:01:34 +0800

    drivers:hwspinlock:support multiple hwlock chips in atlas7
    The atlas7 has two hwspinlock chips in SoC. These two chips
    have different spinlock numbers and base ids.So we place the
    number of spinlocks and base id into DTS node.
    
    One hwspinlock chip will be used by IO-bridge driver.The
    iobridge driver's init level is postcore init, so we make
    hwspinlock driver's init level to core init.
    
    Change-Id: I49f404d462e711046df59013b6e5a9436b4e38ea
    Signed-off-by: Wei Chen <cawei@codeaurora.org>

--- drivers/hwspinlock/sirf_hwspinlock.c
+++ drivers/hwspinlock/sirf_hwspinlock.c
@@ -1,9 +1,16 @@
 /*
  * SIRF hardware spinlock driver
  *
- * Copyright (c) 2015 Cambridge Silicon Radio Limited, a CSR plc group company.
+ * Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
  *
- * Licensed under GPLv2.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #include <linux/module.h>
@@ -18,15 +25,18 @@
 
 struct sirf_hwspinlock {
 	void __iomem *io_base;
+	uint32_t number;
 	struct hwspinlock_device bank;
 };
 
-/* Number of Hardware Spinlocks*/
-#define	HW_SPINLOCK_NUMBER	30
+/* Hardware spinlock control register */
+#define HW_SPINLOCK_CONTROL_REG(base)	(base)
+/* Enable Hardware Spinlocks */
+#define HW_SPINLOCK_ENABLE	0x0
 
-/* Hardware spinlock register offsets */
-#define HW_SPINLOCK_BASE	0x04
-#define HW_SPINLOCK_OFFSET(x)	(HW_SPINLOCK_BASE + 0x4 * (x))
+/* Hardware Spinlock TestAndSet Registers */
+#define HW_SPINLOCK_BASE(base)		(base + 0x04)
+#define HW_SPINLOCK_REG(base, x)	(HW_SPINLOCK_BASE(base) + 0x4 * (x))
 
 static int sirf_hwspinlock_trylock(struct hwspinlock *lock)
 {
@@ -54,13 +64,27 @@ static int sirf_hwspinlock_probe(struct platform_device *pdev)
 	struct sirf_hwspinlock *hwspin;
 	struct hwspinlock *hwlock;
 	struct resource *res;
+	u32 num_of_locks, base_id;
 	int idx, ret;
 
-	if (!pdev->dev.of_node)
+	ret = of_property_read_u32(pdev->dev.of_node,
+			"num-spinlocks", &num_of_locks);
+	if (ret) {
+		dev_err(&pdev->dev,
+			"Unable to find hwspinlock number. ret=%d\n", ret);
 		return -ENODEV;
+	}
+
+	ret = of_property_read_u32(pdev->dev.of_node,
+			"base-id", &base_id);
+	if (ret) {
+		dev_err(&pdev->dev,
+			"Unable to find base id. ret=%d\n", ret);
+		return -ENODEV;
+	}
 
 	hwspin = devm_kzalloc(&pdev->dev, sizeof(*hwspin) +
-			sizeof(*hwlock) * HW_SPINLOCK_NUMBER, GFP_KERNEL);
+			sizeof(*hwlock) * num_of_locks, GFP_KERNEL);
 	if (!hwspin)
 		return -ENOMEM;
 
@@ -70,18 +94,22 @@ static int sirf_hwspinlock_probe(struct platform_device *pdev)
 	if (!hwspin->io_base)
 		return -ENOMEM;
 
-	for (idx = 0; idx < HW_SPINLOCK_NUMBER; idx++) {
+	hwspin->number = num_of_locks;
+	for (idx = 0; idx < hwspin->number; idx++) {
 		hwlock = &hwspin->bank.lock[idx];
-		hwlock->priv = hwspin->io_base + HW_SPINLOCK_OFFSET(idx);
+		hwlock->priv = HW_SPINLOCK_REG(hwspin->io_base, idx);
 	}
+
+	/* Enable hwspinlock */
+	writel(HW_SPINLOCK_ENABLE, HW_SPINLOCK_CONTROL_REG(hwspin->io_base));
 
 	platform_set_drvdata(pdev, hwspin);
 
 	pm_runtime_enable(&pdev->dev);
 
 	ret = hwspin_lock_register(&hwspin->bank, &pdev->dev,
-				   &sirf_hwspinlock_ops, 0,
-				   HW_SPINLOCK_NUMBER);
+				   &sirf_hwspinlock_ops, base_id,
+				   hwspin->number);
 	if (ret)
 		goto reg_failed;
 
@@ -124,8 +152,17 @@ static struct platform_driver sirf_hwspinlock_driver = {
 	},
 };
 
-module_platform_driver(sirf_hwspinlock_driver);
+static int __init sirf_hwspinlock_init(void)
+{
+	return platform_driver_register(&sirf_hwspinlock_driver);
+}
+
+/*
+ * We have to put this driver's init level to core init, because
+ * this driver will be used by iobridge driver. The iobridge
+ * driver's init level is postcore init.
+ */
+core_initcall(sirf_hwspinlock_init);
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("SIRF Hardware spinlock driver");
-MODULE_AUTHOR("Wei Chen <wei.chen@csr.com>");
