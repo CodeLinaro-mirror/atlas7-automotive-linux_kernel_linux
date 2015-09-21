4d1fb5ba56d02b9faf38709cd985c61ff326c404
Guoying Zhang <guoyingz@codeaurora.org>
2015-09-21 16:51:00 +0800

    input: sirfsoc_ts: fix license name error
    Change-Id: Iaed4cb54d5c3be6cc56b8e0b5befa7ed11f06cef
    Signed-off-by: Guoying Zhang <guoyingz@codeaurora.org>

--- drivers/input/touchscreen/sirfsoc_ts.c
+++ drivers/input/touchscreen/sirfsoc_ts.c
@@ -1,9 +1,16 @@
 /*
  * sirfsoc touch controller Driver
  *
- * Copyright (c) 2011 Cambridge Silicon Radio Limited, a CSR plc group company.
+ * Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
  *
- * Licensed under GPLv2 or later.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #include <linux/module.h>
@@ -659,6 +666,5 @@ static struct platform_driver tsc_sirfsoc_driver = {
 
 module_platform_driver(tsc_sirfsoc_driver);
 
-MODULE_AUTHOR("Sober Song <Zhiwu.Song@csr.com>");
 MODULE_DESCRIPTION("SiRF SoC On-chip Touch screen driver");
-MODULE_LICENSE("GPLv2");
+MODULE_LICENSE("GPL v2");
