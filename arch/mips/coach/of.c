518dc43ba833898d820a6ea1cc4df3e108808699
Zhe Jiang <zhej@codeaurora.org>
2016-03-11 16:56:41 +0800

    Add the gplv2 license statement
    Change-Id: If2493ec05e53dfd7ec56100ba423af9010e5f430
    Signed-off-by: Zhe Jiang <zhej@codeaurora.org>

--- arch/mips/coach/of.c
+++ arch/mips/coach/of.c
@@ -1,6 +1,4 @@
 /*
- * This program is free software; you can redistribute it and/or modify it
- *
  * Copyright (c) 2013, 2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
--- drivers/misc/csr_rfkill.c
+++ drivers/misc/csr_rfkill.c
@@ -1,12 +1,17 @@
 /*
-* CSR Synergy for Linux Bluetooth and WLAN Enable Driver
-*
+ * CSR Synergy for Linux Bluetooth and WLAN Enable Driver
+ *
  * Copyright (c) [2014-2016] The Linux Foundation. All rights reserved.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2 as
-* published by the Free Software Foundation.
-*/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
