a83232cc0ae1f8e85666beaf023379fecc941266
Dennis Li <yajunl@codeaurora.org>
2016-04-15 16:19:12 +0800

    sirf:vout Fix issue# interlaced stream has garbage
    Reason: vout driver use weave de-interacling mode as default,
            so the result is bad.
    
    Solution: change the default de-interlacing mode as 3-Median
    
    Change-Id: I9d1ee3ac749650077c12cae518296ff3a7a98503
    Signed-off-by: Dennis Li <yajunl@codeaurora.org>

--- drivers/media/platform/sirfsoc/sirfsoc_vout.c
+++ drivers/media/platform/sirfsoc/sirfsoc_vout.c
@@ -2357,7 +2357,7 @@ static int sirfsoc_vout_create_video_devices(struct platform_device *pdev)
 		vout->display =
 			vid_dev->display[i / SIRFSOC_MAX_VOUT_ON_EACH_DISPLAY];
 
-		vout->di_mode = VDSS_VPP_DI_WEAVE;
+		vout->di_mode = VDSS_VPP_3MEDIAN;
 		sirfsoc_setup_video_data(vout);
 
 		/* the internal index in each display, from 0 to 'MAX -1' */
