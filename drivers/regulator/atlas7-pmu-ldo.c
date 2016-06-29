058652c09daff3e14c140d8886a6f23000a63f7a
Zhiqiang Tu <ztu@codeaurora.org>
2016-06-29 19:27:22 +0800

    regulator: atlas7: add null pointer check
    Change-Id: I93aa4bb59b22be49864d40e07579f2be85ed932b
    Signed-off-by: Zhiqiang Tu <ztu@codeaurora.org>

--- drivers/regulator/atlas7-pmu-ldo.c
+++ drivers/regulator/atlas7-pmu-ldo.c
@@ -120,8 +120,7 @@ static int atlas7_analog_ldo_probe(struct platform_device *pdev)
 
 
 	mem_res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	base = devm_ioremap(&pdev->dev, mem_res->start,
-		resource_size(mem_res));
+	base = devm_ioremap_resource(&pdev->dev, mem_res);
 	if (IS_ERR(base))
 		return PTR_ERR(base);
 
