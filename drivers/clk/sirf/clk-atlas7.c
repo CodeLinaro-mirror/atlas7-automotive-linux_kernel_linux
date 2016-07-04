a2b7a13601405b6c121b079feb9947f8482bf857
Rongjun Ying <rying@codeaurora.org>
2016-07-04 15:33:16 +0800

    clk: CSR: atlas7b: kalimba audio clk enable after the system boot-up, M3 should enable them also
    Change-Id: I0fc996e6d8e2e16c4d80db4f16a925bfdf9b04e1
    Signed-off-by: Rongjun Ying <rying@codeaurora.org>

--- drivers/clk/sirf/clk-atlas7.c
+++ drivers/clk/sirf/clk-atlas7.c
@@ -1642,6 +1642,10 @@ static void __init atlas7_clk_init(struct device_node *np)
 				!strcmp(unit->unit_name, "dmac3_kas") ||
 				!strcmp(unit->unit_name, "ccsec_sec") ||
 				!strcmp(unit->unit_name, "sys2pci_io") ||
+				!strcmp(unit->unit_name, "kas_kas") ||
+				!strcmp(unit->unit_name, "audmscm_nocd") ||
+				!strcmp(unit->unit_name, "gpum_nocd") ||
+				!strcmp(unit->unit_name, "dmac2_kas") ||
 				!strcmp(unit->unit_name, "coresight_cpudiv2"))
 			clk_prepare_enable(atlas7_clks[i]);
 	}
