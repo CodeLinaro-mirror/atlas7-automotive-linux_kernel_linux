1104aa513923920b5a98afaa1ffe7f89a7852064
Rong Wang <rongw@codeaurora.org>
2016-08-24 15:44:44 +0800

    usb: chipidea: init spinlock in core probe
    If the spinlock is initiated in the udc start, while there's
    no udc functionality configured, then there's a warning when
    accessed from the debug interface.
    
    Change-Id: Iccaea43f6802b9351535b4880ddfb21350a76dca
    Signed-off-by: Rong Wang <rongw@codeaurora.org>

--- drivers/usb/chipidea/core.c
+++ drivers/usb/chipidea/core.c
@@ -652,6 +652,8 @@ static int ci_hdrc_probe(struct platform_device *pdev)
 	ci->imx28_write_fix = !!(ci->platdata->flags &
 		CI_HDRC_IMX28_WRITE_FIX);
 
+	spin_lock_init(&ci->lock);
+
 	ret = hw_device_init(ci, base);
 	if (ret < 0) {
 		dev_err(dev, "can't initialize hardware\n");
